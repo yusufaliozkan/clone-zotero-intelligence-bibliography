d477f39dd9d5ea77158c236cd3279c6d3b6bccf9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-13 14:40:01 +0100

    d

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -120,7 +120,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     collection_link = df_collections[df_collections['Collection_Name'] == collection_name]['Collection_Link'].iloc[0]
 
     st.markdown('#### Collection theme: ' + collection_name)
-    col1, col2, col3 = st.columns([1,2,5])
+    col1, col2, col3 = st.columns([1,2,4])
     with col1:
         container_metric = st.container()
     with col2:
