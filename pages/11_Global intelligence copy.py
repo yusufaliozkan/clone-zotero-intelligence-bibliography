f9ee5338e05282c7f24294b5a517d18c06db3c38
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 17:14:04 +0100

    ds

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -224,11 +224,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     container_author_no.metric(label='Number of authors', value=int(author_no))
                     container_country.(label='Number of country', value=unique_items_count-1)
 
-            st.metric(label='Number of country', value=unique_items_count-1)
-            st.metric(label="Number of citations", value=int(citation_count), help='Journal articles only')
-            st.metric(label="Open access coverage", value=f'{int(oa_ratio)}%', help='Journal articles only')
-            st.metric(label='Number of authors', value=int(author_no))
-            st.metric(label='Author/publication ratio', value=author_pub_ratio, help='The average author number per publication')
+            # st.metric(label='Number of country', value=unique_items_count-1)
+            # st.metric(label="Number of citations", value=int(citation_count), help='Journal articles only')
+            # st.metric(label="Open access coverage", value=f'{int(oa_ratio)}%', help='Journal articles only')
+            # st.metric(label='Number of authors', value=int(author_no))
+            # st.metric(label='Author/publication ratio', value=author_pub_ratio, help='The average author number per publication')
 
                     # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
 
