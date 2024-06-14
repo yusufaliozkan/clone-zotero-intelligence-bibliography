120222f0710493c0adbb93a70de8247212de3957
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 17:53:18 +0100

    ds

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -333,6 +333,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     citation_count = df_countries['Citation'].sum()
                     item_type_no = df_countries['Publication type'].nunique()
                     st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
+                    df_countries
 
                     container_metric_2.metric('Number of publications', value=num_items_collections)
                     container_citation_2.metric(label="Number of citations", value=int(citation_count))
