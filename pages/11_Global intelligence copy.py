26cf3095cf439aedf1b8560e320aab93a345be8b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 17:07:01 +0100

    sd

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -208,10 +208,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                     citation_count = df_collections['Citation'].sum()
                     st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
+                    item_type_no = df_collections['Publication type'].nunique()
 
                     container_metric_2.metric('Number of publications', value=num_items_collections)
                     container_citation_2.metric(label="Number of citations", value=int(citation_count))
                     container_oa.metric(label="Open access coverage", value=f'{int(oa_ratio)}%', help='Journal articles only')
+                    container_type.metric(label='Number of publication types', value=int(item_type_no))
 
                     # THIS WAS THE PLACE WHERE FORMAT_ENTRY WAS LOCATED
 
