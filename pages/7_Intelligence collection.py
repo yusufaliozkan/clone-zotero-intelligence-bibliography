3f27326fde365a0618e03c0653893a5885312ae4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 10:58:59 +0100

    ds

--- pages/7_Intelligence collection.py
+++ pages/7_Intelligence collection.py
@@ -75,27 +75,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
     st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
 
     container = st.container()
-
-    query_params = st.query_params.to_dict()
-    selected_collection_key  = query_params.get("collection_id", None)
-
-    unique_collections = list(df_collections['Collection_Name'].unique())
-
-    selected_collection_name = reverse_collection_mapping.get(selected_collection_key, None)
-
-    if selected_collection_name in unique_collections:
-        # Set the default value to the selected collection from the query params
-        radio = container.radio('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
-    else:
-        radio = container.radio('Select a collection', unique_collections)
-
-    # radio = container.radio('Select a collection', unique_collections)
-    # collection_name = st.selectbox('Select a collection:', clist)
+    
     collection_name = radio
     collection_key = collection_mapping[collection_name]
-    # if collection_name:
-    st.query_params.from_dict({"collection_id": collection_key})
-    # st.experimental_set_query_params(collection_name=radio)
 
     df_collections = df_collections.loc[df_collections['Collection_Name']==collection_name]
     pd.set_option('display.max_colwidth', None)
