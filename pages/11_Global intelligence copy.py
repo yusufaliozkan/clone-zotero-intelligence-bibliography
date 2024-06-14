bc88c2ea16d2dba8d2025d0097a3f756d070f946
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 14:40:47 +0100

    ds

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -379,137 +379,137 @@ with st.spinner('Retrieving data & updating dashboard...'):
             unique_continents =  [''] + list(unique_continents)  # Added 'All Countries' option
 
 
-            # Function to update query_params based on selected country
-            def update_params2():
-                if st.session_state.qt:
-                    st.query_params.from_dict({'country': st.session_state.qt})
+            # # Function to update query_params based on selected country
+            # def update_params2():
+            #     if st.session_state.qt:
+            #         st.query_params.from_dict({'country': st.session_state.qt})
 
-            # Retrieve query_params and initialize selected_country
-            query_params2 = st.query_params
-            selected_continent = query_params2.get('continent', '')  # Use .get() to handle None gracefully
+            # # Retrieve query_params and initialize selected_country
+            # query_params2 = st.query_params
+            # selected_continent = query_params2.get('continent', '')  # Use .get() to handle None gracefully
 
-            # Calculate selected_country_index to set the initial index of the selectbox
-            selected_continent_index = unique_continents.index(selected_continent) if selected_continent in unique_continents else 0
+            # # Calculate selected_country_index to set the initial index of the selectbox
+            # selected_continent_index = unique_continents.index(selected_continent) if selected_continent in unique_continents else 0
 
-            # Create selectbox to choose a country
-            selected_continent = st.selectbox('Select a Continent', unique_continents, index=selected_continent_index, on_change=update_params2, key='qt')
+            # # Create selectbox to choose a country
+            # selected_continent = st.selectbox('Select a Continent', unique_continents, index=selected_continent_index, on_change=update_params2, key='qt')
 
-            # Query_params handling based on selected country
-            ix = 0
-            if selected_continent:
-                try:
-                    # Get the index of selected_country in unique_countries
-                    ix = unique_continents.index(selected_continent)
-                except ValueError:
-                    pass
+            # # Query_params handling based on selected country
+            # ix = 0
+            # if selected_continent:
+            #     try:
+            #         # Get the index of selected_country in unique_countries
+            #         ix = unique_continents.index(selected_continent)
+            #     except ValueError:
+            #         pass
 
-            number_of_pub = df_continent[df_continent['Continent'] == selected_continent]
+            # number_of_pub = df_continent[df_continent['Continent'] == selected_continent]
 
-            publications_count = len(number_of_pub)
+            # publications_count = len(number_of_pub)
 
-            number_of_pub_con = df_continent[df_continent['Continent'] == selected_continent]
-            publications_count_con = len(number_of_pub_con)
+            # number_of_pub_con = df_continent[df_continent['Continent'] == selected_continent]
+            # publications_count_con = len(number_of_pub_con)
 
-            df_continent = df_continent[df_continent['Continent'] == selected_continent]  
+            # df_continent = df_continent[df_continent['Continent'] == selected_continent]  
 
-            if not selected_continent or selected_continent=="":
-                st.write('Please select a continent') 
+            # if not selected_continent or selected_continent=="":
+            #     st.write('Please select a continent') 
             
-            else:
-                with st.expander('Click to expand', expanded=True):
-                    st.subheader(f"{selected_continent} ({publications_count_con} sources)")
-                    types = st.multiselect('Publication type', df_continent['Publication type'].unique(),df_continent['Publication type'].unique(), key='original_5')
-                    df_continent = df_continent[df_continent['Publication type'].isin(types)]
-                    df_continent = df_continent.reset_index(drop=True)
-                    df_download_continent = df_continent[['Publication type','Title','FirstName2','Abstract','Date published','Publisher','Journal','Link to publication','Zotero link']]
-                    df_download_continent = df_download_continent.reset_index(drop=True)
-                    def convert_df(df_download_continent):
-                        return df_download_continent.to_csv(index=False).encode('utf-8-sig')
-                    csv = convert_df(df_download_continent)
-                    today = datetime.date.today().isoformat()
-                    a = f'{selected_continent}_{today}'
-                    st.download_button('💾 Download items', csv, (a+'.csv'), mime="text/csv", key='download-csv-6')
+            # else:
+            #     with st.expander('Click to expand', expanded=True):
+            #         st.subheader(f"{selected_continent} ({publications_count_con} sources)")
+            #         types = st.multiselect('Publication type', df_continent['Publication type'].unique(),df_continent['Publication type'].unique(), key='original_5')
+            #         df_continent = df_continent[df_continent['Publication type'].isin(types)]
+            #         df_continent = df_continent.reset_index(drop=True)
+            #         df_download_continent = df_continent[['Publication type','Title','FirstName2','Abstract','Date published','Publisher','Journal','Link to publication','Zotero link']]
+            #         df_download_continent = df_download_continent.reset_index(drop=True)
+            #         def convert_df(df_download_continent):
+            #             return df_download_continent.to_csv(index=False).encode('utf-8-sig')
+            #         csv = convert_df(df_download_continent)
+            #         today = datetime.date.today().isoformat()
+            #         a = f'{selected_continent}_{today}'
+            #         st.download_button('💾 Download items', csv, (a+'.csv'), mime="text/csv", key='download-csv-6')
 
-                    publications_by_type_continent = df_continent['Publication type'].value_counts()
-                    num_items_collections_continent = len(df_continent)
-                    breakdown_string_continent = ', '.join([f"{key}: {value}" for key, value in publications_by_type_continent.items()])                    
+            #         publications_by_type_continent = df_continent['Publication type'].value_counts()
+            #         num_items_collections_continent = len(df_continent)
+            #         breakdown_string_continent = ', '.join([f"{key}: {value}" for key, value in publications_by_type_continent.items()])                    
 
-                    articles_list = []  # Store articles in a list
-                    st.write(f"**{num_items_collections_continent}** sources found ({breakdown_string_continent})")
-                    true_count = df_continent[df_continent['Publication type']=='Journal article']['OA status'].sum()
-                    total_count = len(df_continent[df_continent['Publication type']=='Journal article'])
-                    if total_count == 0:
-                        oa_ratio = 0.0
-                    else:
-                        oa_ratio = true_count / total_count * 100
+            #         articles_list = []  # Store articles in a list
+            #         st.write(f"**{num_items_collections_continent}** sources found ({breakdown_string_continent})")
+            #         true_count = df_continent[df_continent['Publication type']=='Journal article']['OA status'].sum()
+            #         total_count = len(df_continent[df_continent['Publication type']=='Journal article'])
+            #         if total_count == 0:
+            #             oa_ratio = 0.0
+            #         else:
+            #             oa_ratio = true_count / total_count * 100
 
-                    citation_count = df_continent['Citation'].sum()
-                    st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
+            #         citation_count = df_continent['Citation'].sum()
+            #         st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
                 
-                    for index, row in df_continent.iterrows():
-                        formatted_entry = format_entry(row)  # Assuming format_entry() is a function formatting each row
-                        articles_list.append(formatted_entry)        
+            #         for index, row in df_continent.iterrows():
+            #             formatted_entry = format_entry(row)  # Assuming format_entry() is a function formatting each row
+            #             articles_list.append(formatted_entry)        
                     
-                    for index, row in df_continent.iterrows():
-                        publication_type = row['Publication type']
-                        title = row['Title']
-                        authors = row['FirstName2']
-                        date_published = row['Date published']
-                        link_to_publication = row['Link to publication']
-                        zotero_link = row['Zotero link']
+            #         for index, row in df_continent.iterrows():
+            #             publication_type = row['Publication type']
+            #             title = row['Title']
+            #             authors = row['FirstName2']
+            #             date_published = row['Date published']
+            #             link_to_publication = row['Link to publication']
+            #             zotero_link = row['Zotero link']
 
-                        if publication_type == 'Journal article':
-                            published_by_or_in = 'Published in'
-                            published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
-                        elif publication_type == 'Book':
-                            published_by_or_in = 'Published by'
-                            published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
-                        else:
-                            published_by_or_in = ''
-                            published_source = ''
+            #             if publication_type == 'Journal article':
+            #                 published_by_or_in = 'Published in'
+            #                 published_source = str(row['Journal']) if pd.notnull(row['Journal']) else ''
+            #             elif publication_type == 'Book':
+            #                 published_by_or_in = 'Published by'
+            #                 published_source = str(row['Publisher']) if pd.notnull(row['Publisher']) else ''
+            #             else:
+            #                 published_by_or_in = ''
+            #                 published_source = ''
 
-                        formatted_entry = (
-                            '**' + str(publication_type) + '**' + ': ' +
-                            str(title) + ' ' +
-                            '(by ' + '*' + str(authors) + '*' + ') ' +
-                            '(Publication date: ' + str(date_published) + ') ' +
-                            ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
-                            '[[Publication link]](' + str(link_to_publication) + ') ' +
-                            '[[Zotero link]](' + str(zotero_link) + ')'
-                        )
-                    sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation'), key='continent')
-                    display2 = st.checkbox('Display abstracts', key='type_country_999')
-                    if sort_by == 'Publication date :arrow_down:' or df_continent['Citation'].sum() == 0:
-                        count = 1
-                        for index, row in df_continent.iterrows():
-                            formatted_entry = format_entry(row)
-                            st.write(f"{count}) {formatted_entry}")
-                            count += 1
-                            if display2:
-                                st.caption(row['Abstract']) 
-                    elif sort_by == 'Publication type' or df_continent['Citation'].sum() == 0:
-                        df_continent = df_continent.sort_values(by=['Publication type'], ascending=True)
-                        current_type = None
-                        count_by_type = {}
-                        for index, row in df_continent.iterrows():
-                            if row['Publication type'] != current_type:
-                                current_type = row['Publication type']
-                                st.subheader(current_type)
-                                count_by_type[current_type] = 1
-                            formatted_entry = format_entry(row)
-                            st.write(f"{count_by_type[current_type]}) {formatted_entry}")
-                            count_by_type[current_type] += 1
-                            if display2:
-                                st.caption(row['Abstract'])
-                    else:
-                        df_continent = df_continent.sort_values(by=['Citation'], ascending=False)
-                        count = 1
-                        for index, row in df_continent.iterrows():
-                            formatted_entry = format_entry(row)
-                            st.write(f"{count}) {formatted_entry}")
-                            count += 1
-                            if display2:
-                                st.caption(row['Abstract']) 
+            #             formatted_entry = (
+            #                 '**' + str(publication_type) + '**' + ': ' +
+            #                 str(title) + ' ' +
+            #                 '(by ' + '*' + str(authors) + '*' + ') ' +
+            #                 '(Publication date: ' + str(date_published) + ') ' +
+            #                 ('(' + published_by_or_in + ': ' + '*' + str(published_source) + '*' + ') ' if published_by_or_in else '') +
+            #                 '[[Publication link]](' + str(link_to_publication) + ') ' +
+            #                 '[[Zotero link]](' + str(zotero_link) + ')'
+            #             )
+            #         sort_by = st.radio('Sort by:', ('Publication date :arrow_down:', 'Publication type',  'Citation'), key='continent')
+            #         display2 = st.checkbox('Display abstracts', key='type_country_999')
+            #         if sort_by == 'Publication date :arrow_down:' or df_continent['Citation'].sum() == 0:
+            #             count = 1
+            #             for index, row in df_continent.iterrows():
+            #                 formatted_entry = format_entry(row)
+            #                 st.write(f"{count}) {formatted_entry}")
+            #                 count += 1
+            #                 if display2:
+            #                     st.caption(row['Abstract']) 
+            #         elif sort_by == 'Publication type' or df_continent['Citation'].sum() == 0:
+            #             df_continent = df_continent.sort_values(by=['Publication type'], ascending=True)
+            #             current_type = None
+            #             count_by_type = {}
+            #             for index, row in df_continent.iterrows():
+            #                 if row['Publication type'] != current_type:
+            #                     current_type = row['Publication type']
+            #                     st.subheader(current_type)
+            #                     count_by_type[current_type] = 1
+            #                 formatted_entry = format_entry(row)
+            #                 st.write(f"{count_by_type[current_type]}) {formatted_entry}")
+            #                 count_by_type[current_type] += 1
+            #                 if display2:
+            #                     st.caption(row['Abstract'])
+            #         else:
+            #             df_continent = df_continent.sort_values(by=['Citation'], ascending=False)
+            #             count = 1
+            #             for index, row in df_continent.iterrows():
+            #                 formatted_entry = format_entry(row)
+            #                 st.write(f"{count}) {formatted_entry}")
+            #                 count += 1
+            #                 if display2:
+            #                     st.caption(row['Abstract']) 
 
             col11, col12 = st.columns([3,2])
             with col11:                
