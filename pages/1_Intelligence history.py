abdc306f0487639eead9032b1d8ca8ba6ec2cfa8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-14 08:20:27 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -88,7 +88,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
         # Set the default value to the selected collection from the query params
         radio = container.selectbox('Select a collection', unique_collections, index=unique_collections.index(selected_collection_name))
     else:
-        radio = container.radio('Select a collection', unique_collections)
+        radio = container.selectbox('Select a collection', unique_collections)
 
     # radio = container.radio('Select a collection', unique_collections)
     # collection_name = st.selectbox('Select a collection:', clist)
