dc54ceaf9c0b6bd88111b8ce8149f4e537ed4b15
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-15 12:33:52 +0100

    sd

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -117,7 +117,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             st.metric(label='Number of authors', value=int(author_no))
             st.metric(label='Author/publication ratio', value=author_pub_ratio, help='The average author number per publication')
 
-            container_metric = container_metric.metric(label='Number of items in this collection', value=num_items_collections, help=f'sources found for **{unique_items_count-1}** countries.')
+            container_metric = container_metric.metric(label='Number of items', value=num_items_collections, help=f'sources found for **{unique_items_count-1}** countries.')
 
     df_countries['Date published'] = ( 
         df_countries['Date published']
