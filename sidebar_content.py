5def6ddc9e05e74c28fd97daf7df269e6246ff55
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-16 09:15:05 +0100

    sd

--- sidebar_content.py
+++ sidebar_content.py
@@ -2,6 +2,8 @@ import streamlit as st
 import streamlit.components.v1 as components
 from copyright import display_custom_license, cc_by_licence_image
 
+st.toast('Join our [mailing list](https://groups.google.com/g/intelligence-studies-network) to receive updates.')
+
 def sidebar_content():
     image = 'https://images.pexels.com/photos/315918/pexels-photo-315918.png'
     with st.sidebar:
