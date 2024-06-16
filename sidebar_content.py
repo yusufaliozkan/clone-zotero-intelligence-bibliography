1cccc439c50bf7f9fba1a559eff408682d3a61b8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-16 09:18:39 +0100

    z

--- sidebar_content.py
+++ sidebar_content.py
@@ -42,3 +42,5 @@ def sidebar_content():
             Report your technical issues or requests [here](https://github.com/YusufAliOzkan/zotero-intelligence-bibliography/issues).            
             ''')
         st.write('Check the digest [here](https://intelligence.streamlit.app/Digest)')
+        st.toast('Join our [mailing list](https://groups.google.com/g/intelligence-studies-network) to receive updates.')
+
