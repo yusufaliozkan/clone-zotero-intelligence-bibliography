900ed3836730b67cba9437546ac38d8d07fa54bc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-17 14:53:02 +0100

    as

--- Home.py
+++ Home.py
@@ -2860,4 +2860,4 @@ with st.spinner('Retrieving data & updating dashboard...'):
             all_bibliographies += f'{response.text}\n\n'  # Print error response for debugging
 
     # Display all bibliographies in Streamlit
-    st.markdown(bibliography, unsafe_allow_html=True)
+    st.markdown(all_bibliographies, unsafe_allow_html=True)
