06fc8a0dc60204a45c358b9d9f80d2fa8a620b87
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-17 14:07:12 +0100

    sd

--- Home.py
+++ Home.py
@@ -2834,7 +2834,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     base_url = 'https://api.zotero.org'
 
     # List of item keys you want to retrieve
-    ['ZWRFYLAW', 'QPGERC3Q', 'D4TFCPGK', '94EQKK5L']
+    item_keys = ['ZWRFYLAW', 'QPGERC3Q', 'D4TFCPGK', '94EQKK5L']
 
     # Initialize an empty string to accumulate bibliographies
     all_bibliographies = ""
