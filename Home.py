4dca8471d6b1952211c3f08414922fb572bedf18
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-17 15:12:34 +0100

    sd

--- Home.py
+++ Home.py
@@ -2845,7 +2845,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
         # Parameters for the request
         params = {
             'format': 'atom',
-            'content':'bib'
+            'content':'citation'
         }
 
         # Make GET request to Zotero API
@@ -2860,4 +2860,4 @@ with st.spinner('Retrieving data & updating dashboard...'):
             all_bibliographies += f'{response.text}\n\n'  # Print error response for debugging
 
         # Display all bibliographies in Streamlit
-        st.markdown(bibliography, unsafe_allow_html=True)
+        st.markdown(all_bibliographies, unsafe_allow_html=True)
