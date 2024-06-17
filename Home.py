47f50f4a1da364b295df40135cd0de6b82044ed6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-17 13:30:05 +0100

    sd

--- Home.py
+++ Home.py
@@ -37,15 +37,6 @@ from format_entry import format_entry
 from streamlit_dynamic_filters import DynamicFilters
 # from rss_feed import df_podcast, df_magazines
 
-citation_html333 = '''
-<div class="csl-bib-body" style="line-height: 2; padding-left: 1em; text-indent:-1em;">
-  <div class="csl-entry"><i>&#x201C;U.S. Army Open-Source Intelligence (OSINT)&#x201D; &#x2013; with Dennis Eger &amp; Shawn Nilius</i>. (2024, June 11). <a href="https://thecyberwire.com/podcasts/spycast/637/notes">https://thecyberwire.com/podcasts/spycast/637/notes</a></div>
-</div>
-'''
-
-# Display the citation in Streamlit
-st.markdown(citation_html333, unsafe_allow_html=True)
-
 # Connecting Zotero with API 
 library_id = '2514686'
 library_type = 'group'
@@ -2834,3 +2825,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
         ''') 
 
     display_custom_license()
+
+    citation_html333 = '''
+    <div class="csl-bib-body" style="line-height: 2; padding-left: 1em; text-indent:-1em;">
+    <div class="csl-entry"><i>&#x201C;U.S. Army Open-Source Intelligence (OSINT)&#x201D; &#x2013; with Dennis Eger &amp; Shawn Nilius</i>. (2024, June 11). <a href="https://thecyberwire.com/podcasts/spycast/637/notes">https://thecyberwire.com/podcasts/spycast/637/notes</a></div>
+    </div>
+    '''
+
+    # Display the citation in Streamlit
+    st.markdown(citation_html333, unsafe_allow_html=True)
