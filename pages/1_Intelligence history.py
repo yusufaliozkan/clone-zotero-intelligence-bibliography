7091d0d860bf52b36a06bd326e42b217fbad793c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-18 14:06:35 +0100

    ds

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -319,7 +319,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     df_table_view
                 else:
                     df_table_view
-            elif cited_view:
+            else:
                 df_collections['zotero_item_key'] = df_collections['Zotero link'].str.replace('https://www.zotero.org/groups/intelligence_bibliography/items/', '')
                 df_zotero_id = pd.read_csv('zotero_citation_format.csv')
                 df_collections = pd.merge(df_collections, df_zotero_id, on='zotero_item_key', how='left')
