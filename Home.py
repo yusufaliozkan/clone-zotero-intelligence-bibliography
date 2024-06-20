9bad12633754ae2320bb200f8fc9f5eec1a3f2cc
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 09:59:51 +0100

    sd

--- Home.py
+++ Home.py
@@ -799,7 +799,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             container_metric = st.container()
                         with col2:
                             with st.popover('More metrics'):
-                                container_citation.metric(label="Number of citations", value=int(citation_count))
+                                container_citation = st.metric()
 
                         st.write('*Please note that this database **may not show** all research outputs of the author.*')
                         types = st.multiselect('Publication type', filtered_collection_df_authors['Publication type'].unique(), filtered_collection_df_authors['Publication type'].unique(), key='original_authors')
