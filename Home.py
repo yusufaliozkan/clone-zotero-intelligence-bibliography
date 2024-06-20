4be1b98f28eb09cc0fc542c3cf3a0398ecc66d18
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 12:58:50 +0100

    sd

--- Home.py
+++ Home.py
@@ -237,7 +237,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             st.metric(
                 label="Number of citations", 
                 value=int(citation_count), 
-                help=f'Not all papers are tracked for citation. Citation mean: **{round(citation_mean, 1)}**, Citation median: **{round(citation_median, 1)}**'
+                help=f'Not all papers are tracked for citation. Citation per publication: **{round(citation_mean, 1)}**, Citation median: **{round(citation_median, 1)}**'
                 )
 
             true_count = df_dedup[df_dedup['Publication type']=='Journal article']['OA status'].sum()
