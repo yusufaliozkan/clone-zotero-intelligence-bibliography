ccb82288a69caf436e6e9afbc68a2e00973f28a9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 11:48:46 +0100

    sd

--- Home.py
+++ Home.py
@@ -840,7 +840,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                         citation_count = filtered_collection_df_authors['Citation'].sum()
                         container_citation.metric(label="Number of citations", value=int(citation_count))
-                        container_collaboration_ratio.metric(label='Citation per publication', value=citation_count/num_items_collections)
+                        container_collaboration_ratio.metric(label='Citation per publication', value=round(citation_count/num_items_collections, 0))
 
                         st.write(f'Number of citations: **{int(citation_count)}**, Open access coverage (journal articles only): **{int(oa_ratio)}%**')
 
