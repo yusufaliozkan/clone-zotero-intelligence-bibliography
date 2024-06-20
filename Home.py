757a07f742c558538b8497b15c603d79f626051d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 14:19:55 +0100

    sd

--- Home.py
+++ Home.py
@@ -424,7 +424,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
             search_option = st.radio("Select search option", ("Search keywords", "Search author", "Search collection", "Publication types", "Search journal", "Publication year", "Cited papers"))
 
             if search_option == "Search keywords":
-                st.subheader('Search keywords', anchor=False, help='Check guidance')
+                st.subheader('Search keywords', anchor=False)
                 @st.experimental_dialog("Search guide")
                 def guide(item):
                     st.write('''
