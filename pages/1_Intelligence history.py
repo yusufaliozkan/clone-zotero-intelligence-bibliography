7591657791fff3012aa4183e105baef5c287cbe4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 20:53:19 +0100

    sa

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -208,11 +208,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
             citation_count = df_collections['Citation'].sum()
 
-            df_collections['FirstName2'] = df_collections['FirstName2'].astype(str)
-            df_collections['multiple_authors'] = df_collections['FirstName2'].apply(lambda x: ',' in x)
-            multiple_authored_papers = df_collections['multiple_authors'].sum()
-            collaboration_ratio = round(multiple_authored_papers / item_count * 100, 1)
-            container_publication_ratio.metric(label='Collaboration ratio', value=f'{(collaboration_ratio)}%', help='Ratio of multiple-authored papers')
 
             a = f'{collection_name}_{today}'
             st.download_button('💾 Download the collection', csv, (a+'.csv'), mime="text/csv", key='download-csv-4')
@@ -223,6 +218,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
     container_type.metric(label='Number of publication types', value=int(item_type_no))
     container_author_no.metric(label='Number of authors', value=int(author_no))
     container_author_pub_ratio.metric(label='Author/publication ratio', value=author_pub_ratio, help='The average author number per publication')
+
+    df_collections['FirstName2'] = df_collections['FirstName2'].astype(str)
+    df_collections['multiple_authors'] = df_collections['FirstName2'].apply(lambda x: ',' in x)
+    multiple_authored_papers = df_collections['multiple_authors'].sum()
+    collaboration_ratio = round(multiple_authored_papers / num_items_collections * 100, 1)
+    container_publication_ratio.metric(label='Collaboration ratio', value=f'{(collaboration_ratio)}%', help='Ratio of multiple-authored papers')
 
     tab1, tab2 = st.tabs(['📑 Publications', '📊 Dashboard'])
     with tab1:
