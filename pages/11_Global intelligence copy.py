fc169b80ec90615a2398db8a7bfd09e56406ce05
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-20 22:09:49 +0100

    sd

--- pages/11_Global intelligence copy.py
+++ pages/11_Global intelligence copy.py
@@ -517,10 +517,10 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         else:
                             df_table_view                                 
                     else:
-                        df_collections['zotero_item_key'] = df_collections['Zotero link'].str.replace('https://www.zotero.org/groups/intelligence_bibliography/items/', '')
+                        df_countries['zotero_item_key'] = df_countries['Zotero link'].str.replace('https://www.zotero.org/groups/intelligence_bibliography/items/', '')
                         df_zotero_id = pd.read_csv('zotero_citation_format.csv')
-                        df_collections = pd.merge(df_collections, df_zotero_id, on='zotero_item_key', how='left')
-                        df_zotero_id = df_collections[['zotero_item_key']]
+                        df_countries = pd.merge(df_countries, df_zotero_id, on='zotero_item_key', how='left')
+                        df_zotero_id = df_countries[['zotero_item_key']]
 
                         def display_bibliographies(df):
                             all_bibliographies = ""
