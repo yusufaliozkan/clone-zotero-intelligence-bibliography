02112e26577f29c53d25f2b89e790bc01de20cc5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 16:23:54 +0100

    sd

--- Home.py
+++ Home.py
@@ -2741,7 +2741,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 grouped = df_dedup_v2.groupby('Date year')
                 df_citation_counts = df_dedup_v2.copy()
                 df_citation_counts["Citations from OA outputs"] = df_citation_counts.apply(lambda row: row["Citation"] if row["OA status"] == True else 0, axis=1)
-                df_citation_counts = df_citation_counts.groupby("Publication_year").agg({"Citation": "sum", "Citations from OA outputs": "sum"}).reset_index()
+                df_citation_counts = df_citation_counts.groupby("Date year").agg({"Citation": "sum", "Citations from OA outputs": "sum"}).reset_index()
                 df_citation_counts["Citations from non-OA outputs"] = df_citation_counts['Citation']-df_citation_counts["Citations from OA outputs"]
                 df_citation_counts["%Citations from OA outputs"] = round(df_citation_counts['Citations from OA outputs']/df_citation_counts['Citation'], 3)*100
                 df_citation_counts["%Citations from non-OA outputs"] = round(df_citation_counts['Citations from non-OA outputs']/df_citation_counts['Citation'], 3)*100
