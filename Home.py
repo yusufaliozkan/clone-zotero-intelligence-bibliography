95c45e5e5840c6f9719531c561daea3b5e163899
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 16:56:41 +0100

    sd

--- Home.py
+++ Home.py
@@ -2746,7 +2746,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 filtered_df = df_dedup_v2[(df_dedup_v2['Citation status'] == True) & (df['OA status'] == True)]
 
                 # Group by 'Date year' and count the number of rows in each group
-                filtered_df
+                result = filtered_df.groupby(df_dedup_v2['Date year'])['OA status'].count()
+                result
 
                 total_citations = grouped.size().reset_index(name='Total Publications')
                 cited_publications = grouped['Citation status'].apply(lambda x: (x == True).sum()).reset_index(name='Cited papers')
