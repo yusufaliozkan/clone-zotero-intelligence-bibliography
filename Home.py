4fed5b18b3363a0fb22f0785d3d7f6ce96f94cb6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 11:47:29 +0100

    ss

--- Home.py
+++ Home.py
@@ -1730,7 +1730,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             st.metric(label=f"The number of sources published between **{int(years[0])}** and **{int(years[1])}**", value=f'{number_of_items}', label_visibility='visible', 
                             help=f'({breakdown_string})')    
                         with colyear22:
-                            true_count = df_all[df_all['Publication type']=='Journal article']['OA status'].sum()
+                            true_count = df_all['OA status'].sum()
+                            true_count
                             total_count = len(df_all[df_all['Publication type']=='Journal article'])
                             if total_count == 0:
                                 oa_ratio = 0.0
