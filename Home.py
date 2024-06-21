91a584df11b4c1f45ca2ee025c3bc523dd821aa9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 16:22:29 +0100

    sd

--- Home.py
+++ Home.py
@@ -2745,8 +2745,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_citation_counts["Citations from non-OA outputs"] = df_citation_counts['Citation']-df_citation_counts["Citations from OA outputs"]
                 df_citation_counts["%Citations from OA outputs"] = round(df_citation_counts['Citations from OA outputs']/df_citation_counts['Citation'], 3)*100
                 df_citation_counts["%Citations from non-OA outputs"] = round(df_citation_counts['Citations from non-OA outputs']/df_citation_counts['Citation'], 3)*100
+                df_citation_counts = df_citation_counts[['%Citations from OA outputs', '%Citations from non-OA outputs']]
                 df_citation_counts
-
                 total_publications = grouped.size().reset_index(name='Total Publications')
                 open_access_publications = grouped['OA status'].apply(lambda x: (x == True).sum()).reset_index(name='OA Publications')
                 df_oa_overtime = pd.merge(total_publications, open_access_publications, on='Date year')
