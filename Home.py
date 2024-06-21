b064486ebbe0e3a7fbbcf40661ea2ecbe6395318
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 15:09:36 +0100

    sd

--- Home.py
+++ Home.py
@@ -2743,12 +2743,17 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 total_publications = grouped.size().reset_index(name='TotalPublications')
                 open_access_publications = grouped['OA status'].apply(lambda x: (x == True).sum()).reset_index(name='OpenAccessPublications')
                 df_oa_overtime = pd.merge(total_publications, open_access_publications, on='Date year')
-                df_oa_overtime['NonOpenAccessPublications'] = df_oa_overtime['TotalPublications']-df_oa_overtime['OpenAccessPublications']
                 df_oa_overtime['OA publication ratio'] = round(df_oa_overtime['OpenAccessPublications']/df_oa_overtime['TotalPublications'], 3)*100
                 df_oa_overtime['Non-OA publication ratio'] = 100-df_oa_overtime['OA publication ratio']
                 max_year = df_oa_overtime["Date year"].max()
                 df_oa_overtime
-
+                last_20_years = df_oa_overtime[df_oa_overtime["Date year"] >= (max_year - 20)]
+                fig = px.bar(last_20_years, x="Date year", y=["OA publication ratio", "Non-OA publication ratio"],
+                            labels={"Date year": "Publication Year", "value": "Percentage (%)", "variable": "Type"},
+                            title="Open Access Publications Ratio Over the Last 20 Years",
+                            color_discrete_map={"OA publication ratio": "green", "Non-OA publication ratio": "#D3D3D3"},
+                            barmode="stack")
+                st.plotly_chart(fig, use_container_width = True)
 
                 col1, col2 = st.columns([7,2])
                 with col1:
