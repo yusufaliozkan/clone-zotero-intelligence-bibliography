94f15cb3addbec1dd2d1fe433296e5155952ee48
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-21 16:09:14 +0100

    d

--- Home.py
+++ Home.py
@@ -2738,8 +2738,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_dedup['Date year'] = df_dedup['Date published2'].dt.strftime('%Y')
                 df_dedup['Date year'] = pd.to_numeric(df_dedup['Date year'], errors='coerce', downcast='integer')
                 df_dedup_v2 = df_dedup.dropna(subset='OA status')
-                df_dedup_v2
                 grouped = df_dedup_v2.groupby('Date year')
+                df_citation_counts = df_dedup_v2.groupby("Publication_year")["Citation"].sum().reset_index()
+                df_citation_counts
                 total_publications = grouped.size().reset_index(name='Total Publications')
                 open_access_publications = grouped['OA status'].apply(lambda x: (x == True).sum()).reset_index(name='OA Publications')
                 df_oa_overtime = pd.merge(total_publications, open_access_publications, on='Date year')
