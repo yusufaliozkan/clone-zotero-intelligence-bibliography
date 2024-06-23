f21aa4c4b4862b5980a30f8008754c99ef53a627
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 00:00:58 +0100

    rdf

--- Home.py
+++ Home.py
@@ -2852,13 +2852,10 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                 df_oa_papers_citation_count = filtered_df.groupby(df_dedup_v2['Date year'])['Citation'].sum().reset_index()
                 df_oa_papers_citation_count.columns = ['Date year', '#Citations (OA papers)']
-                df_oa_papers_citation_count['#Citations (OA papers)'] = df_oa_papers_citation_count['#Citations (OA papers)'].fillna(0)
-                df_oa_papers_citation_count
                 df_citation_count = filtered_df2.groupby(df_dedup_v2['Date year'])['Citation'].sum().reset_index()
                 df_citation_count.columns = ['Date year', '#Citations (all)']
-                df_citation_count
                 df_citation_count = pd.merge(df_citation_count, df_oa_papers_citation_count, on='Date year', how='left')
-                df_citation_count
+                df_citation_count['#Citations (OA papers)'] = df_citation_count['#Citations (OA papers)'].fillna(0)
                 df_citation_count['#Citations (non-OA papers)'] = df_citation_count['#Citations (all)'] - df_citation_count['#Citations (OA papers)']
                 df_citation_count['%Citation count (OA papers)'] = round(df_citation_count['#Citations (OA papers)']/df_citation_count['#Citations (all)'], 3)*100
                 df_citation_count['%Citation count (non-OA papers)'] = round(df_citation_count['#Citations (non-OA papers)']/df_citation_count['#Citations (all)'], 3)*100
