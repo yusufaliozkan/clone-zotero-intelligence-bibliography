df5e175e70d881a95ae57d1cc55836ad8a2c58eb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 19:32:52 +0100

    sd

--- Home.py
+++ Home.py
@@ -2609,11 +2609,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     col1, col2 = st.columns(2)
                     with col1:
                             table_view = st.checkbox('Table view')
+                            df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
+                            df_authors = df_authors[df_authors['Author_name'] != 'nan']
+                            df_authors = df_authors['Author_name'].value_counts().head(num_authors)
+                            df_authors = df_authors.reset_index()
                             if not table_view:           
-                                df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
-                                df_authors = df_authors[df_authors['Author_name'] != 'nan']
-                                df_authors = df_authors['Author_name'].value_counts().head(num_authors)
-                                df_authors = df_authors.reset_index()
+
                                 df_authors = df_authors.rename(columns={'index':'Author','Author_name':'Number of Publications'})
                                 fig = px.bar(df_authors, x=df_authors['Author'], y=df_authors['Number of Publications'])
                                 fig.update_layout(
@@ -2627,12 +2628,13 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                 df_authors
                     with col2:
                             selected_type = st.radio('Select a publication type', ['Journal article', 'Book', 'Book chapter'])
+                            df_authors = df_csv.copy()              
+                            df_authors = df_authors[df_authors['Publication type']==selected_type]
+                            df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
+                            df_authors = df_authors.explode('Author_name')
+                            df_authors.reset_index(drop=True)
                             if not table_view: 
-                                df_authors = df_csv.copy()              
-                                df_authors = df_authors[df_authors['Publication type']==selected_type]
-                                df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
-                                df_authors = df_authors.explode('Author_name')
-                                df_authors.reset_index(drop=True)
+
                                 df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name'])
                                 df_authors = df_authors[df_authors['Author_name'] != 'nan']
                                 df_authors = df_authors['Author_name'].value_counts().head(num_authors)
