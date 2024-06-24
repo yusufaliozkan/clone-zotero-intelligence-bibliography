e5cfa95adffaaddad0b0c6d6996c4b882fa0710e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 23:22:13 +0100

    sd

--- Home.py
+++ Home.py
@@ -2841,7 +2841,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_oa_overtime['OA publication ratio'] = round(df_oa_overtime['#OA Publications']/df_oa_overtime['Total Publications'], 3)*100
                 df_oa_overtime['Non-OA publication ratio'] = 100-df_oa_overtime['OA publication ratio']
                 df_oa_overtime = pd.merge(df_oa_overtime, df_cited_papers, on='Date year')
-                col1, col2 = st.columns(2)
+                col1, col2 = st.columns([3,1])
                 with col1:
                     @st.experimental_fragment
                     def fragment2():
