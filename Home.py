965fc520a9940cabe367be74713ed2225de54d0c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 12:49:48 +0100

    sd

--- Home.py
+++ Home.py
@@ -2466,7 +2466,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         df_collections_2 = df_collections_2[df_collections_2['Date year'] !='No date']
                         filter_collection = (df_collections_2['Date year'].astype(int)>=years[0]) & (df_collections_2['Date year'].astype(int)<years[1])
                         df_collections_2 = df_collections_2.loc[filter_collection]
-            fragment_filter()
+            fragment_filter(df_csv, df_collections_2, min_y, max_y)
             if df_csv['Title'].any() in ("", [], None, 0, False):
                 st.warning('No data to visualise. Select a correct parameter.')
 
