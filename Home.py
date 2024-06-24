dbdb862cdb44053a47d1b6364441144f0bece443
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 14:18:27 +0100

    ds

--- Home.py
+++ Home.py
@@ -2578,10 +2578,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     col1.plotly_chart(fig, use_container_width = True)
 
                 with col2:
-                    df_authors = df_csv.copy()
+                    
 
                     @st.experimental_fragment
-                    def author_chart():                        
+                    def author_chart():
+                        df_authors = df_csv.copy()                 
                         df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
                         df_authors = df_authors.explode('Author_name')
                         df_authors.reset_index(drop=True, inplace=True)
