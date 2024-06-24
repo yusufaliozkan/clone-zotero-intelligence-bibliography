63fb00ab8b7727813688651b3165da0672bede1a
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 14:38:18 +0100

    ds

--- Home.py
+++ Home.py
@@ -2595,6 +2595,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                         df_authors = df_authors.reset_index()
                         df_authors
                         df_authors = df_authors.rename(columns={'index':'Author','Author_name':'Number of Publications'})
+                        df_authors
                         fig = px.bar(df_authors, x=df_authors.index, y=df_authors.values)
                         fig.update_layout(
                             title=f'Top {num_authors} Authors by Publication Count',
