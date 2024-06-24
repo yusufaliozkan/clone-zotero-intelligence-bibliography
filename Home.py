92ec4ab48f20afd6485334058435decd77756f76
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-24 18:50:33 +0100

    sd

--- Home.py
+++ Home.py
@@ -2593,6 +2593,12 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
                 col1, col2 = st.columns(2)
                 with col1:
+                    df_year
+                    max_y = int(df_year['Publication year'].max())
+                    min_y = int(df_year['Publication year'].min())
+                    years = st.slider('Publication years between:', min_y, max_y+1, (min_y,max_y+1), key='years3')
+                    filter = (df_year['Date year'].astype(int)>=years[0]) & (df_year['Date year'].astype(int)<years[1])
+                    df_year = df_year.loc[filter]
                     fig = px.bar(df_year, x='Publication year', y='Count')
                     fig.update_xaxes(tickangle=-70)
                     fig.update_layout(
