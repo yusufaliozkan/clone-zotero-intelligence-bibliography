0141d981549fbe9213c6c409c7d68a7a3546f019
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-25 19:28:55 +0100

    sd

--- Home.py
+++ Home.py
@@ -277,7 +277,6 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_dedup['FirstName2'] = df_dedup['FirstName2'].astype(str)
             df_dedup['multiple_authors'] = df_dedup['FirstName2'].apply(lambda x: ',' in x)
             multiple_authored_papers = df_dedup['multiple_authors'].sum()
-            multiple_authored_papers
             collaboration_ratio = round(multiple_authored_papers / item_count * 100, 1)
             st.metric(label='Collaboration ratio', value=f'{(collaboration_ratio)}%', help='Ratio of multiple-authored papers')
 
@@ -2661,73 +2660,73 @@ with st.spinner('Retrieving data & updating dashboard...'):
                                     df_authors.columns = ['Author name', 'Publication count']
                                     df_authors
 
-                    df_authors3 = df_authors2.copy()
-                    grouped_3 = df_authors3.groupby('Date year')
-                    total_publications_3 = grouped_3.size().reset_index(name='Total Publications')
-                    multiple_authored_papers_3 = grouped_3['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='# Multiple Authored Publications')
-                    df_multiple_authors_3 = pd.merge(total_publications_3, multiple_authored_papers_3, on='Date year')
-                    df_multiple_authors_3['# Single Authored Publications'] = df_multiple_authors_3['Total Publications']- df_multiple_authors_3['# Multiple Authored Publications']
+                    col1, col2 = st.columns(2)
+                    with col1:
+                        df_authors3 = df_authors2.copy()
+                        grouped_3 = df_authors3.groupby('Date year')
+                        total_publications_3 = grouped_3.size().reset_index(name='Total Publications')
+                        multiple_authored_papers_3 = grouped_3['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='# Multiple Authored Publications')
+                        df_multiple_authors_3 = pd.merge(total_publications_3, multiple_authored_papers_3, on='Date year')
+                        df_multiple_authors_3['# Single Authored Publications'] = df_multiple_authors_3['Total Publications']- df_multiple_authors_3['# Multiple Authored Publications']
 
-                    df_authors2 = df_authors2.copy()
-                    # df_authors2['Date published2'] = (
-                    #     df_authors2['Date published']
-                    #     # .str.strip()
-                    #     .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
-                    # )
-                    df_authors2['Date year'] = df_authors2['Date published'].dt.strftime('%Y')
-                    df_authors2['Date year'] = pd.to_numeric(df_authors2['Date year'], errors='coerce', downcast='integer')
-                    grouped = df_authors2.groupby('Date year')
-                    total_publications = grouped.size().reset_index(name='Total Publications')
-                    multiple_authored_papers = grouped['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='# Multiple Authored Publications')
-                    df_multiple_authors = pd.merge(total_publications, multiple_authored_papers, on='Date year')
-                    df_multiple_authors['# Single Authored Publications'] = df_multiple_authors['Total Publications']- df_multiple_authors['# Multiple Authored Publications']
-                    df_multiple_authors = df_multiple_authors[df_multiple_authors['Date year']!='No date']
-                    df_multiple_authors['% Multiple Authored Publications'] = round(df_multiple_authors['# Multiple Authored Publications']/df_multiple_authors['Total Publications'], 3)*100
-                    df_multiple_authors['% Single Authored Publications'] = round(df_multiple_authors['# Single Authored Publications']/df_multiple_authors['Total Publications'], 3)*100
-                    current_year = datetime.datetime.now().year
-                    df_multiple_authors = df_multiple_authors[df_multiple_authors['Date year']<=current_year]
+                        df_authors2 = df_authors2.copy()
+                        # df_authors2['Date published2'] = (
+                        #     df_authors2['Date published']
+                        #     # .str.strip()
+                        #     .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
+                        # )
+                        df_authors2['Date year'] = df_authors2['Date published'].dt.strftime('%Y')
+                        df_authors2['Date year'] = pd.to_numeric(df_authors2['Date year'], errors='coerce', downcast='integer')
+                        grouped = df_authors2.groupby('Date year')
+                        total_publications = grouped.size().reset_index(name='Total Publications')
+                        multiple_authored_papers = grouped['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='# Multiple Authored Publications')
+                        df_multiple_authors = pd.merge(total_publications, multiple_authored_papers, on='Date year')
+                        df_multiple_authors['# Single Authored Publications'] = df_multiple_authors['Total Publications']- df_multiple_authors['# Multiple Authored Publications']
+                        df_multiple_authors = df_multiple_authors[df_multiple_authors['Date year']!='No date']
+                        df_multiple_authors['% Multiple Authored Publications'] = round(df_multiple_authors['# Multiple Authored Publications']/df_multiple_authors['Total Publications'], 3)*100
+                        df_multiple_authors['% Single Authored Publications'] = round(df_multiple_authors['# Single Authored Publications']/df_multiple_authors['Total Publications'], 3)*100
+                        current_year = datetime.datetime.now().year
+                        df_multiple_authors = df_multiple_authors[df_multiple_authors['Date year']<=current_year]
 
-                    max_year = df_multiple_authors["Date year"].max()
-                    last_20_years = df_multiple_authors[df_multiple_authors["Date year"] >= (max_year - 20)]
-                    see_number_pubs = st.toggle('See number of publications')
+                        max_year = df_multiple_authors["Date year"].max()
+                        last_20_years = df_multiple_authors[df_multiple_authors["Date year"] >= (max_year - 20)]
+                        see_number_pubs = st.toggle('See number of publications')
 
-                    fig1 = go.Figure()
-                    fig1.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['# Multiple Authored Publications'], mode='lines+markers', name='# Multiple Authored Publications'))
-                    fig1.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['# Single Authored Publications'], mode='lines+markers', name='# Single Authored Publications'))
+                        fig1 = go.Figure()
+                        fig1.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['# Multiple Authored Publications'], mode='lines+markers', name='# Multiple Authored Publications'))
+                        fig1.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['# Single Authored Publications'], mode='lines+markers', name='# Single Authored Publications'))
 
-                    fig1.update_layout(title='# Single vs Multiple Authored Publications Over the Years',
-                                    xaxis_title='Year',
-                                    yaxis_title='Number of Publications',
-                                    template='plotly_white')
+                        fig1.update_layout(title='# Single vs Multiple Authored Publications Over the Years',
+                                        xaxis_title='Year',
+                                        yaxis_title='Number of Publications',
+                                        template='plotly_white')
 
-                    fig2 = go.Figure()
-                    fig2.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['% Multiple Authored Publications'], mode='lines+markers', name='% Multiple Authored Publications'))
-                    fig2.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['% Single Authored Publications'], mode='lines+markers', name='% Single Authored Publications'))
+                        fig2 = go.Figure()
+                        fig2.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['% Multiple Authored Publications'], mode='lines+markers', name='% Multiple Authored Publications'))
+                        fig2.add_trace(go.Scatter(x=last_20_years['Date year'], y=last_20_years['% Single Authored Publications'], mode='lines+markers', name='% Single Authored Publications'))
 
-                    fig2.update_layout(title='% Single vs Multiple Authored Publications Over the Years',
-                                    xaxis_title='Publication Year',
-                                    yaxis_title='% Publications',
-                                    template='plotly_white')
+                        fig2.update_layout(title='% Single vs Multiple Authored Publications Over the Years',
+                                        xaxis_title='Publication Year',
+                                        yaxis_title='% Publications',
+                                        template='plotly_white')
 
-                    if see_number_pubs:
-                        st.plotly_chart(fig1, use_container_width=True)
-                    else:
-                        st.plotly_chart(fig2, use_container_width=True)
-
-                    multiple_authored = df_multiple_authors_3['# Multiple Authored Publications'].sum()
-                    single_authored = df_multiple_authors_3['# Single Authored Publications'].sum()
-                    labels = ['Multiple Authored Publications', 'Single Authored Publications']
-                    values = [multiple_authored, single_authored]
-                    custom_colors = ['#D3D3D3', 'green'] 
-                    fig = px.pie(
-                        values=values,
-                        names=labels,
-                        title='Single vs Multiple Authored Papers',
-                        color_discrete_sequence=custom_colors
-                    )
-                    st.plotly_chart(fig)
-
-
+                        if see_number_pubs:
+                            st.plotly_chart(fig1, use_container_width=True)
+                        else:
+                            st.plotly_chart(fig2, use_container_width=True)
+                    with col2:
+                        multiple_authored = df_multiple_authors_3['# Multiple Authored Publications'].sum()
+                        single_authored = df_multiple_authors_3['# Single Authored Publications'].sum()
+                        labels = ['Multiple Authored Publications', 'Single Authored Publications']
+                        values = [multiple_authored, single_authored]
+                        custom_colors = ['blue', 'red'] 
+                        fig = px.pie(
+                            values=values,
+                            names=labels,
+                            title='Single vs Multiple Authored Papers',
+                            color_discrete_sequence=custom_colors
+                        )
+                        st.plotly_chart(fig)
                 author_chart()
 
                 st.divider()
