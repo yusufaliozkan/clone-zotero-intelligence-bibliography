b884f5ca9563992c2f8451e59f49c9116806bac9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-25 09:11:57 +0100

    sd

--- Home.py
+++ Home.py
@@ -3010,7 +3010,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                             xaxis_title="Publication Year",
                             yaxis_title="Citation Count",
                         )
-                        line_show = st.toggle("See citation count")
+                        line_show = st.toggle("Citation count graph")
                         if line_show:
                             st.plotly_chart(fig_line, use_container_width=True)
                         else:
