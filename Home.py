04d81c1e1880b39991eb31e2b2306c7650d12145
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-25 14:39:03 +0100

    ds

--- Home.py
+++ Home.py
@@ -2826,6 +2826,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                 df_cited_oa_papers = filtered_df.groupby(df_dedup_v2['Date year'])['OA status'].count()
                 df_cited_oa_papers=df_cited_oa_papers.reset_index()
                 df_cited_oa_papers.columns = ['Date year', 'Cited OA papers']
+                df_cited_oa_papers
                 filtered_df2 = df_dedup_v2[(df_dedup_v2['Citation status'] == True)]
                 filtered_df2
 
