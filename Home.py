285f5ea3359a7868d4e70c9e825783b0059a5a45
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-25 18:35:36 +0100

    dr

--- Home.py
+++ Home.py
@@ -2604,9 +2604,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     # df_multiple_authors = df_authors[df_authors['multiple_authors']==True]
                     grouped = df_authors.groupby('Date year')
                     total_publications = grouped.size().reset_index(name='Total Publications')
-                    total_publications
                     multiple_authored_papers = grouped['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='Multiple authored papers')
-                    multiple_authored_papers
                     df_multiple_authors = pd.merge(total_publications, multiple_authored_papers, on='Date year')
                     df_multiple_authors
                     df_authors['Author_name'] = df_authors['FirstName2'].apply(lambda x: x.split(', ') if isinstance(x, str) and x else x)
