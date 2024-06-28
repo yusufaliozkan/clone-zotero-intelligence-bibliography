2dd5a64ed7bc6aeb98e76c081434306902d44350
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-28 20:05:29 +0100

    sd

--- Home.py
+++ Home.py
@@ -54,7 +54,7 @@ pd.set_option('display.max_colwidth', None)
 pages = [
         st.Page('Home.py', title='Home page'),
         st.Page('1_Intelligence history.py', title='History'),
-        st.Page('2_Intelligence studies.py', title='Intel studies')
+        st.Page('pages/2_Intelligence studies.py', title='Intel studies')
     ]
 
 pg = st.navigation(pages)
