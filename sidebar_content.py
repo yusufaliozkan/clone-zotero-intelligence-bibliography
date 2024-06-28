140cdb43a12b6995e662625c5db768cbbf7b61fb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-28 17:29:17 +0100

    sa

--- sidebar_content.py
+++ sidebar_content.py
@@ -4,8 +4,7 @@ from copyright import display_custom_license, cc_by_licence_image
 
 def sidebar_content():
     image = 'https://images.pexels.com/photos/315918/pexels-photo-315918.png'
-
-
+    
     with st.sidebar:
         st.image(image, width=150)
         st.sidebar.markdown("# Intelligence studies network")
