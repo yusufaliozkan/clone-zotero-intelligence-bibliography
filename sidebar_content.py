f9e0b289ce51eda004ffa0c9b99a377194d89abb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-06-28 17:14:23 +0100

    sd

--- sidebar_content.py
+++ sidebar_content.py
@@ -6,7 +6,7 @@ def sidebar_content():
     image = 'https://images.pexels.com/photos/315918/pexels-photo-315918.png'
 
     p1 = st.Page("Home.py")
-    p2 = st.Page('1_Intelligence history')
+    p2 = st.Page('1_Intelligence history.py')
     p3 = st.Page('2_Intelligence studies.py')
 
     pg = st.navigation({'Home':[p1], 'Collections':[p2, p3]})
