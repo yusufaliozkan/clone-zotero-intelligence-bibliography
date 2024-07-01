8a577bf85cad7ed3932f4a41fc5a72ad03d6f558
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-01 18:35:39 +0100

    sd

--- format_entry.py
+++ format_entry.py
@@ -63,13 +63,13 @@ def format_entry(row, include_citation=True):
         # )
     else:
         return (
-            f"**{publication_type}**: {title} " \
-            f"(by *{authors}*) " \
-            f"(Publication date: {date_published}) " \
-            f"({published_by_or_in}: *{published_source}*) " if published_by_or_in else '' + \
-            f"[[Publication link]]({link_to_publication}) " \
-            f"[[Zotero link]]({zotero_link}) " \
-            f"{oa_link_text} " if oa_link_text else '' + \
+            f"**{publication_type}**: {title} "
+            f"(by *{authors}*) "
+            f"(Publication date: {date_published}) "
+            f"({published_by_or_in}: *{published_source}*) " if published_by_or_in else '' 
+            f"[[Publication link]]({link_to_publication}) "
+            f"[[Zotero link]]({zotero_link}) "
+            f"{oa_link_text} " if oa_link_text else '' 
             f"{citation_text}" if include_citation else ''
         )
             # '**' + publication_type + '**' + ': ' +
