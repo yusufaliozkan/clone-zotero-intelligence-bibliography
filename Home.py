5dce326ed93805252a8a5e742c4a742844cdbb40
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-01 20:09:12 +0100

    sd

--- Home.py
+++ Home.py
@@ -1283,7 +1283,7 @@ with st.spinner('Retrieving data...'):
                     # publications_by_type = filtered_collection_df['Publication type'].value_counts()
                     
                     with st.expander('Click to expand', expanded=True):
-                        st.subheader('Publications by ' + selected_type, anchor=False, divider='blue')
+                        st.subheader('Publication type: ' + selected_type, anchor=False, divider='blue')
                         if selected_type == 'Thesis':
                             st.warning('Links to PhD theses catalouged by the British EThOS may not be working due to the [cyber incident at the British Library](https://www.bl.uk/cyber-incident/).')
                         
