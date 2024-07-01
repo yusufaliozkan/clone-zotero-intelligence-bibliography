911f58e3985347213c333b2bb73b3df8e3448102
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-01 18:25:22 +0100

    sd

--- Home.py
+++ Home.py
@@ -212,6 +212,7 @@ with st.spinner('Retrieving data...'):
 
     df_dedup = pd.read_csv('all_items.csv')
     df_duplicated = pd.read_csv('all_items_duplicated.csv')
+    df_dedup
 
     col1, col2, col3 = st.columns([3,5,8])
     with col3:
