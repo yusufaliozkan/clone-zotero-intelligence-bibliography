898a5eba3919e40e1fdda3c1728a9dc89eb22005
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-01 10:46:46 +0100

    sd

--- Home.py
+++ Home.py
@@ -82,7 +82,7 @@ def zotero_data(library_id, library_type):
         ))
     df = pd.DataFrame(data, columns=columns)
     return df
-zotero_data
+items
 df = zotero_data(library_id, library_type)
 
 df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
