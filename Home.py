9b306c1cff259b195eb185fd25ee13dfbaa86bcb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-01 10:24:22 +0100

    sd

--- Home.py
+++ Home.py
@@ -282,7 +282,11 @@ with st.spinner('Retrieving data & updating dashboard...'):
             df_dedup['multiple_authors'] = df_dedup['FirstName2'].apply(lambda x: ',' in x)
             multiple_authored_papers = df_dedup['multiple_authors'].sum()
             collaboration_ratio = round(multiple_authored_papers / item_count * 100, 1)
-            st.metric(label='Collaboration ratio', value=f'{(collaboration_ratio)}%', help='Ratio of multiple-authored papers')
+            st.metric(
+                label='Collaboration ratio', 
+                value=f'{(collaboration_ratio)}%', 
+                help='Ratio of multiple-authored papers (theses are excluded as they are inherently single-authored publications).'
+            )
 
     sidebar_content() 
 
