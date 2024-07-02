b66d8bf96702286aa84a4b5cfafbb7da046b982b
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-02 16:32:30 +0100

    sd

--- Home.py
+++ Home.py
@@ -28,6 +28,8 @@ import PIL
 from PIL import Image, ImageDraw, ImageFilter
 import json
 from authors_dict import df_authors, name_replacements
+from authors_dict import name_replacements
+
 from copyright import display_custom_license
 from sidebar_content import sidebar_content
 import plotly.graph_objs as go
