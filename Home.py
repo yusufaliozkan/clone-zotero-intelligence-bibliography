8333c772f2c47bb2fc23d7a9931e6b9f333edcf8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-02 13:32:25 +0100

    ds

--- Home.py
+++ Home.py
@@ -486,13 +486,14 @@ with st.spinner('Retrieving data...'):
 
                         # Text input for search keywords
                         with cola:
-                            st.text_input(
-                                'Search keywords in titles or abstracts',
-                                st.session_state.search_term_input,
-                                key='search_term_input',
-                                placeholder='Type your keyword(s)',
-                                on_change=update_search_params
-                            )
+                            st.write('text')
+                            # st.text_input(
+                            #     'Search keywords in titles or abstracts',
+                            #     st.session_state.search_term_input,
+                            #     key='search_term_input',
+                            #     placeholder='Type your keyword(s)',
+                            #     on_change=update_search_params
+                            # )
 
                         # Function to extract quoted phrases
                         def extract_quoted_phrases(text):
