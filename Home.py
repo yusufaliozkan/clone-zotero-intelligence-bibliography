a3efb54f7133ab4d9dd3f70c7408616477453530
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-02 16:08:48 +0100

    sd

--- Home.py
+++ Home.py
@@ -2772,6 +2772,7 @@ with st.spinner('Retrieving data...'):
 
                         grouped_3 = df_authors3.groupby('Date year')
                         total_publications_3 = grouped_3.size().reset_index(name='Total Publications')
+                        multiple_authored_papers_3['multiple_authors'] = multiple_authored_papers_3['FirstName2'].apply(lambda x: ',' in x)
                         multiple_authored_papers_3 = grouped_3['multiple_authors'].apply(lambda x: (x == True).sum()).reset_index(name='# Multiple Authored Publications')
                         df_multiple_authors_3 = pd.merge(total_publications_3, multiple_authored_papers_3, on='Date year')
                         df_multiple_authors_3['# Single Authored Publications'] = df_multiple_authors_3['Total Publications']- df_multiple_authors_3['# Multiple Authored Publications']
