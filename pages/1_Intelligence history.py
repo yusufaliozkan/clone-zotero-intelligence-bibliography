853e222b57ac47c8c56061e8df9c0882cbe4a65d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-05 14:44:36 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -220,7 +220,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
     container_citation_average = citation_count/citeable_items
     container_citation_average
 
-    average = int(df_collections['Citation']).average()
+    average = df_collections['Citation'].average()
+    average = int(average)
     average
 
 
