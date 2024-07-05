e4c549a9ee92104fe4ad4e2c702015d2ccfe192d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-05 15:31:48 +0100

    sd

--- pages/3_Intelligence analysis.py
+++ pages/3_Intelligence analysis.py
@@ -171,7 +171,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
     container_author_no.metric(label='Number of authors', value=int(author_no))
     container_author_pub_ratio.metric(label='Author/publication ratio', value=author_pub_ratio, help='The average author number per publication')
 
-    df_collections['Citation'] = df_collections[df_collections['Citation']<1000]
+    df_collections['Citation'] = df_collections[df_collections['Citation']>1000]
     citation_average = round(df_collections['Citation'].mean(), 2)
     container_citation_average.metric(label="Average citation", value=citation_average)
 
