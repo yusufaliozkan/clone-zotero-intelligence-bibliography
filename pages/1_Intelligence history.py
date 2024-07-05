19a334175070621c549e0ffc83a30b6da0b2b476
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-05 14:09:49 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -216,9 +216,8 @@ with st.spinner('Retrieving data & updating dashboard...'):
     container_metric.metric(label="Items found", value=num_items_collections, help=breakdown_string)
     container_citation.metric(label="Number of citations", value=int(citation_count))
 
-    df_collections
     citeable_items = df_collections['ID'].count()
-    citeable_items
+    container_citation_average = citation_count/citeable_items
 
     container_oa.metric(label="Open access coverage", value=f'{int(oa_ratio)}%', help='Journal articles only')
     container_type.metric(label='Number of publication types', value=int(item_type_no))
