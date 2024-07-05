15fd5946174442ea035a7acf9f056a6873387e7c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-05 16:14:52 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -217,6 +217,18 @@ with st.spinner('Retrieving data & updating dashboard...'):
     container_metric.metric(label="Items found", value=num_items_collections, help=breakdown_string)
     container_citation.metric(label="Number of citations", value=int(citation_count))
 
+    outlier_detector = (df_collections['Citation'] > 1000).any()
+    if outlier_detector == True:
+        outlier_count = (df_collections['Citation'] > 1000).sum()
+        citation_average = df_collections[df_collections['Citation'] < 1000]
+        citation_average = round(citation_average['Citation'].mean(), 2)
+        citation_average_with_outliers = round(df_collections['Citation'].mean(), 2)
+        container_citation_average.metric(
+            label="Average citation", 
+            value=citation_average, 
+            help=f'**{outlier_count}** item(s) passed the threshold of 1000 citations. With the outliers, the average citation count is **{citation_average_with_outliers}**.'
+            )
+
     citation_average = round(df_collections['Citation'].mean(), 2)
     container_citation_average.metric(label="Average citation", value=citation_average)
 
