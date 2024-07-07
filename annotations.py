0b5240177793160c1fe3098d5eccc04757e97367
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-07 19:00:03 +0100

    sd

--- annotations.py
+++ annotations.py
@@ -1,4 +1,5 @@
 def get_copyright_annotation():
+    cc_by_image_url  = "https://i.creativecommons.org/l/by/4.0/80x15.png"
     return [
         dict(
             x=0.1,
@@ -6,7 +7,10 @@ def get_copyright_annotation():
             xref='paper',
             yref='paper',
             showarrow=False,
-            text="© 2024 Yusuf Ozkan. All rights reserved.",
+            text=(
+                "© 2024 Yusuf Ozkan. All rights reserved. "
+                '<img src="' + cc_by_image_url + '" style="height:20px; vertical-align:middle;">'
+            ),
             xanchor='center',
             yanchor='top',
             font=dict(
