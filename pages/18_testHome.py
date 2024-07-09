6a2e1d7cbf473d656b82345d3c3807a07aff27a1
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-09 22:57:08 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -820,11 +820,8 @@ with st.spinner('Retrieving data...'):
                                         return highlighted_text
                                                                 
                                     if view == 'Basic list':
-                                        show_first_20 = st.checkbox("Show only first 20 items (untick to see all)", value=True)
-                                        
-                                        if show_first_20:
-                                            filtered_df = filtered_df.head(20)
-                                            for i, article in enumerate(articles_list[:20], start=1):
+                                        if num_items < 20:
+                                            for i, article in enumerate(articles_list, start=1):
                                                 highlighted_article = highlight_terms(article, search_tokens)
                                                 st.markdown(f"{i}. {highlighted_article}", unsafe_allow_html=True)
                                                 
@@ -839,29 +836,48 @@ with st.spinner('Retrieving data...'):
                                                     else:
                                                         st.caption(f"Abstract: No abstract")
                                         else:
-                                            num_tabs = (num_items // 20) + 1 if num_items > 0 else 1
-                                            tab_titles = [f"Results {i*20+1}-{min((i+1)*20, num_items)}" for i in range(num_tabs)]
+                                            show_first_20 = st.checkbox("Show only first 20 items (untick to see all)", value=True)
                                             
-                                            tabs = st.tabs(tab_titles)
-                                            for tab_index, tab in enumerate(tabs):
-                                                with tab:
-                                                    start_idx = tab_index * 20
-                                                    end_idx = min(start_idx + 20, num_items)
-                                                    for i in range(start_idx, end_idx):
-                                                        article = articles_list[i]
-                                                        highlighted_article = highlight_terms(article, search_tokens)
-                                                        st.markdown(f"{i + 1}. {highlighted_article}", unsafe_allow_html=True)
-                                                        
-                                                        if display_abstracts:
-                                                            abstract = abstracts_list[i]
-                                                            if pd.notnull(abstract):
-                                                                if search_in == 'Title and abstract':
-                                                                    highlighted_abstract = highlight_terms(abstract, search_tokens)
-                                                                else:
-                                                                    highlighted_abstract = abstract 
-                                                                st.caption(f"Abstract: {highlighted_abstract}", unsafe_allow_html=True)
+                                            if show_first_20:
+                                                filtered_df = filtered_df.head(20)
+                                                for i, article in enumerate(articles_list[:20], start=1):
+                                                    highlighted_article = highlight_terms(article, search_tokens)
+                                                    st.markdown(f"{i}. {highlighted_article}", unsafe_allow_html=True)
+                                                    
+                                                    if display_abstracts:
+                                                        abstract = abstracts_list[i - 1]
+                                                        if pd.notnull(abstract):
+                                                            if search_in == 'Title and abstract':
+                                                                highlighted_abstract = highlight_terms(abstract, search_tokens)
                                                             else:
-                                                                st.caption(f"Abstract: No abstract")
+                                                                highlighted_abstract = abstract 
+                                                            st.caption(f"Abstract: {highlighted_abstract}", unsafe_allow_html=True)
+                                                        else:
+                                                            st.caption(f"Abstract: No abstract")
+                                            else:
+                                                num_tabs = (num_items // 20) + 1 if num_items > 0 else 1
+                                                tab_titles = [f"Results {i*20+1}-{min((i+1)*20, num_items)}" for i in range(num_tabs)]
+                                                
+                                                tabs = st.tabs(tab_titles)
+                                                for tab_index, tab in enumerate(tabs):
+                                                    with tab:
+                                                        start_idx = tab_index * 20
+                                                        end_idx = min(start_idx + 20, num_items)
+                                                        for i in range(start_idx, end_idx):
+                                                            article = articles_list[i]
+                                                            highlighted_article = highlight_terms(article, search_tokens)
+                                                            st.markdown(f"{i + 1}. {highlighted_article}", unsafe_allow_html=True)
+                                                            
+                                                            if display_abstracts:
+                                                                abstract = abstracts_list[i]
+                                                                if pd.notnull(abstract):
+                                                                    if search_in == 'Title and abstract':
+                                                                        highlighted_abstract = highlight_terms(abstract, search_tokens)
+                                                                    else:
+                                                                        highlighted_abstract = abstract 
+                                                                    st.caption(f"Abstract: {highlighted_abstract}", unsafe_allow_html=True)
+                                                                else:
+                                                                    st.caption(f"Abstract: No abstract")
                                     if view == 'Table':
                                         df_table_view = filtered_df[['Publication type','Title','Date published','FirstName2', 'Abstract','Publisher','Journal','Collection_Name','Link to publication','Zotero link']]
                                         df_table_view = df_table_view.rename(columns={'FirstName2':'Author(s)','Collection_Name':'Collection','Link to publication':'Publication link'})
