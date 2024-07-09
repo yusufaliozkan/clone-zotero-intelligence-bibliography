7baf5b9878c0fd420dd96060aa1647494ca48088
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-09 11:02:17 +0100

    ds

--- Home.py
+++ Home.py
@@ -591,19 +591,19 @@ with st.spinner('Retrieving data...'):
                                     container_citation = st.container()
                                     container_citation_average = st.container()
                             with colsearch3:
-                            with st.popover("Filters and more"):
-                                types2 = st.multiselect('Publication types', types, key='original2')
-                                collections = st.multiselect('Collection', collections, key='original_collection')
-                                container_download_button = st.container()
+                                with st.popover("Filters and more"):
+                                    types2 = st.multiselect('Publication types', types, key='original2')
+                                    collections = st.multiselect('Collection', collections, key='original_collection')
+                                    container_download_button = st.container()
 
-                                display_abstracts = st.checkbox('Display abstracts')
-                                only_citation = st.checkbox('Show cited items only')
-                                if only_citation:
-                                    filtered_df = filtered_df[(df_csv['Citation'].notna()) & (filtered_df['Citation'] != 0)]
+                                    display_abstracts = st.checkbox('Display abstracts')
+                                    only_citation = st.checkbox('Show cited items only')
+                                    if only_citation:
+                                        filtered_df = filtered_df[(df_csv['Citation'].notna()) & (filtered_df['Citation'] != 0)]
 
-                                view = st.radio('View as:', ('Basic list', 'Table',  'Bibliography'))
-                                # with col114:
-                                #     table_view = st.checkbox('See results in table')
+                                    view = st.radio('View as:', ('Basic list', 'Table',  'Bibliography'))
+                                    # with col114:
+                                    #     table_view = st.checkbox('See results in table')
 
                             if types2:
                                 filtered_df = filtered_df[filtered_df['Publication type'].isin(types2)]                 
