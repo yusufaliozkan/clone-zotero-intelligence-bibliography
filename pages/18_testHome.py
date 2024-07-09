e1098d6450bb7df68d33ce40ead3715ddae1177e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-09 15:25:41 +0100

    ds

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -820,7 +820,9 @@ with st.spinner('Retrieving data...'):
                                         return highlighted_text
 
                                     if view == 'Basic list':
-                                        show_first_20 = st.checkbox("Show only first 20 items (untick to see all)", value=True)
+                                        if 'show_first_20' not in st.session_state:
+                                            st.session_state.show_first_20 = True
+                                        show_first_20 = st.checkbox("Show only first 20 items (untick to see all)", value=st.session_state.show_first_20)
                                         
                                         if show_first_20:
                                             filtered_df = filtered_df.head(20)
