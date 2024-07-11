47cb5d8123e65a074872b273c914ec73759aa5b6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-11 17:07:12 +0100

    ds

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -1932,7 +1932,11 @@ with st.spinner('Retrieving data...'):
                         # publications_by_type = filtered_collection_df['Publication type'].value_counts()
                         
                         with st.expander('Click to expand', expanded=True):
-                            st.markdown('#### Journal: ' + str(journals))
+                            if len(selected_journal_df)=1:
+                                st.markdown('#### Journal: ' + str(journals))
+                            else:
+                                st.markdown('#### Journals: ')
+                                st.caption(f'{str{journals}}')
 
                             coljournal1, coljournal2, coljournal3, coljournal4 = st.columns(4)
                             with coljournal1:
