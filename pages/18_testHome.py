6ed60187592aa477beda0bfd5b4d02d8e72b7a70
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-12 09:11:16 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -2225,7 +2225,7 @@ with st.spinner('Retrieving data...'):
                                         formatted_entry = format_entry(row)
                                         st.write(f"{index + 1}) {formatted_entry}")
                                 if view == 'Table':
-                                    df_table_view = selected_journal_df[['Publication type','Title','Date published','FirstName2', 'Abstract','Link to publication','Zotero link']]
+                                    df_table_view = selected_journal_df[['Publication type','Title', 'Journal', 'Date published','FirstName2', 'Abstract','Link to publication','Zotero link']]
                                     df_table_view = df_table_view.rename(columns={'FirstName2':'Author(s)','Collection_Name':'Collection','Link to publication':'Publication link'})
                                     df_table_view
                                 if view =='Bibliography':
