4965e858da73788008c9dea8ffdeb99ed480d6b6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-13 23:41:14 +0100

    ds

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -98,11 +98,11 @@ with st.spinner('Preparing digest...'):
                 previous_custom = today - dt.timedelta(days=number)
                 rg = previous_custom
             if range_day == 'Custom (select date)':
-                rg = st.date_input('From:', today-dt.timedelta(days=7), max_value=latest_added_date)
+                rg = st.date_input('From:', today-dt.timedelta(days=7), max_value=latest_added_date-1)
                 today = st.date_input('To:', today, max_value=today, min_value=rg)
                 a = today - rg
                 a = str(a.days) + ' days'
-        
+
             filter = (df_csv['Date added']>rg) & (df_csv['Date added']<=today)
             rg2 = rg.strftime('%d/%m/%Y')
             df_csv = df_csv.loc[filter]
