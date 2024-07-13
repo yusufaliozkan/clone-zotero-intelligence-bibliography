07c072f13b477451f3cd187a9903c5df41185e63
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-13 23:46:49 +0100

    sd

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -98,7 +98,7 @@ with st.spinner('Preparing digest...'):
                 previous_custom = today - dt.timedelta(days=number)
                 rg = previous_custom
             if range_day == 'Custom (select date)':
-                rg = st.date_input('From:', today-dt.timedelta(days=7), max_value=latest_added_date-1)
+                rg = st.date_input('From:', today-dt.timedelta(days=7), max_value=(latest_added_date- timedelta(days=1)))
                 today = st.date_input('To:', today, max_value=today, min_value=rg)
                 a = today - rg
                 a = str(a.days) + ' days'
