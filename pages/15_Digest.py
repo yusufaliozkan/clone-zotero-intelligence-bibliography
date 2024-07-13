dede141ecb443a5756b2389dec7c951784a1fa73
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-13 21:41:57 +0100

    sd

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -108,9 +108,8 @@ with st.spinner('Preparing digest...'):
             df_csv = df_csv.loc[filter]
 
             df_csv['Date published'] = pd.to_datetime(df_csv['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
-            df_csv['Date published'] = pd.to_datetime(df_csv['Date published'], errors='coerce').dt.date
 
-            df_csv['Date published new'] = df_csv['Date published'].dt.strftime('%d/%m/%Y')
+            df_csv['Date published'] = df_csv['Date published'].dt.strftime('%d/%m/%Y')
             df_csv['Date months'] = df_csv['Date published'].dt.strftime('%Y-%m')
             df_csv['Date published'] = df_csv['Date published'].fillna('No date')
             # df_csv.sort_values(by='Date published', ascending = False, inplace=True)    
