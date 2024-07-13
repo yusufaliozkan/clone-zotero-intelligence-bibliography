3a561142b1f122b2366a2ea2198645fb3efce0e2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-14 00:11:30 +0100

    sd

--- pages/15_Digest.py
+++ pages/15_Digest.py
@@ -109,7 +109,6 @@ with st.spinner('Preparing digest...'):
                 filter = (df_csv['Date added']>rg) & (df_csv['Date added']<=today)
             rg2 = rg.strftime('%d/%m/%Y')
             df_csv = df_csv.loc[filter]
-            rg
 
             df_csv['Date published'] = pd.to_datetime(df_csv['Date published'],utc=True, errors='coerce').dt.tz_convert('Europe/London')
 
