d3c81e7502cb4748f3a30365d1be3db2f01c059e
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-15 21:26:12 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -795,6 +795,9 @@ with st.spinner('Retrieving data...'):
                                     a = 'search-result-' + today
                                     container_download_button.download_button('💾 Download search', csv, (a+'.csv'), mime="text/csv", key='download-csv-1')
 
+                                    if len(filtered_df) >= len(filtered_df)+1:
+                                        st.rerun()
+
                                     on = st.toggle('Generate dashboard')
 
                                     if on and len(filtered_df) > 0:
