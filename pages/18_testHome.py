49939ffc914dce2bb2507dc25c9a8b3e2043d710
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-15 16:00:16 +0100

    sd

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -431,10 +431,10 @@ with st.spinner('Retrieving data...'):
                     highlighted_text = highlighted_text.replace(f'___URL_PLACEHOLDER_{index}___', url)
                 
                 return highlighted_text
-                
+
             @st.experimental_fragment
             def text_search():
-                name = st.text_input("Enter city name", "").strip()
+                name = st_keyup("Enter city name", "").strip()
                 
                 @st.cache_data
                 def get_titles():
