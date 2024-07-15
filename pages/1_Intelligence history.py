d47b4e9baed34927fd74da6ac6396261863ba593
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-15 23:27:15 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -126,7 +126,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
 
     st.markdown('#### Collection theme: ' + collection_name)
 
-    name = st.text_input("Enter keywords to search in title", key='name')#, debounce=250, key='name')
+    name = st.text_input("Enter keywords to search in title", key='name', placeholder='Search keyword(s)')#, debounce=250, key='name')
     if name:
         df_collections = df_collections[df_collections.Title.str.lower().str.contains(name.lower(), na=False)]
 
