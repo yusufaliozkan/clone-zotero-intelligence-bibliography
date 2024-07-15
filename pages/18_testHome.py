1ae52e38b48980b294feee0ead959f55c2b6b299
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-15 22:22:39 +0100

    ds

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -510,6 +510,8 @@ with st.spinner('Retrieving data...'):
                 search_option = st.radio("Select search option", ("Search keywords", "Search author", "Search collection", "Publication types", "Search journal", "Publication year", "Cited papers"))
                 if search_option == "Search keywords":
                     st.subheader('Search keywords', anchor=False, divider='blue')
+                    search_term = ""
+
                     @st.experimental_fragment
                     def search_keyword(): 
                         @st.experimental_dialog("Search guide")
