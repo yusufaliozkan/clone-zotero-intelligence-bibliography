b82c2d176a9ab4b28b79fff5d7c2493eeb4396d6
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-16 10:40:59 +0100

    ds

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -498,7 +498,7 @@ with st.spinner('Retrieving data...'):
             # Example Streamlit code for context
             st.header('Search in database', anchor=False)
             st.write('<style>div.row-widget.stRadio > div{flex-direction:row;}</style>', unsafe_allow_html=True)
-            @st.experimental_fragment
+            # @st.experimental_fragment
             def search_options_main_menu():
                 from authors_dict import name_replacements
                 total_rows = len(df_dedup)
