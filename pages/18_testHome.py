cb4d4dd37946b7be63b4dd27931e9e0a5763d82f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-17 08:45:59 +0100

    ds

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -1388,7 +1388,6 @@ with st.spinner('Retrieving data...'):
                         numeric_start_collections = df_csv_collections[df_csv_collections['Collection_Name'].str[0].str.isdigit()]['Collection_Name'].unique()
                         all_unique_collections = df_csv_collections['Collection_Name'].unique()
                         filtered_collections = [col for col in numeric_start_collections if col not in excluded_collections]
-                        filtered_collections
         
                         # def remove_numbers(name):
                         #     return re.sub(r'^\d+(\.\d+)*\s*', '', name)
