7853873f770e89a13a8b4adeaf958770f6b2a0fb
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-07-26 12:39:25 +0100

    df

--- pages/18_testHome.py
+++ pages/18_testHome.py
@@ -209,7 +209,7 @@ df = df.fillna('')
 # Streamlit app
 
 st.title("Intelligence studies networks", anchor=False)
-st.header('Intelligence studies bibliography', anchor=False)
+st.header('Intelligence studies bibliographys', anchor=False)
 # st.header("[Zotero group library](https://www.zotero.org/groups/2514686/intelligence_bibliography/library)")
 
 # cite_today = datetime.date.today().isoformat()
