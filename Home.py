793faa24079bb52b5a060b2c92c0ff33c9696b53
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-02 13:46:59 +0100

    sd

--- Home.py
+++ Home.py
@@ -4,9 +4,9 @@ import pandas as pd
 # home_page = st.Page('Home.py', title='Affiliation finder')
 
 home = st.Page('Home_page.py', title='Home')
-intelligence_history = st.Page('pages/1_Intelligence_history.py', title='Intelligence history')
-intelligence_studies = st.Page('pages/2_Intelligence_studies.py', title='Intelligence studies')
-intelligence_analysis = st.Page('pages/3_Intelligence_analysis.py', title='Intelligence analysis')
+intelligence_history = st.Page('pages/1_Intelligence history.py', title='Intelligence history')
+intelligence_studies = st.Page('pages/2_Intelligence studies.py', title='Intelligence studies')
+intelligence_analysis = st.Page('pages/3_Intelligence analysis.py', title='Intelligence analysis')
 
 pg = st.navigation(
     {
