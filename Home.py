207002f26838095b7a16f19507a61c0393331536
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-05 12:04:03 +0100

    as

--- Home.py
+++ Home.py
@@ -15,7 +15,7 @@ counterintelligence = st.Page('pages/8_Counterintelligence.py')
 covert_action = st.Page('pages/9_Covert action.py')
 intelligence_cybersphere = st.Page('pages/10_Intelligence and cybersphere.py')
 global_intelligence = st.Page('pages/11_Global intelligence.py')
-special_collections = st.Page('pages/12_Special collections.py')
+special_collections = st.Page('pages/13_Special collections.py')
 
 pg = st.navigation(
     {
