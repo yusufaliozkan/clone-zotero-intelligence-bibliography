a8b3a728f1481be5c0c2daf9a6085df225bb16c9
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-05 14:50:06 +0100

    ds

--- Home_page.py
+++ Home_page.py
@@ -981,6 +981,7 @@ with st.spinner('Retrieving data...'):
                                                 for i, article in enumerate(articles_list, start=1):
                                                     highlighted_article = highlight_terms(article, search_tokens)
                                                     st.markdown(f"{i}. {highlighted_article}", unsafe_allow_html=True)
+                                                    st.button('test')
                                                     
                                                     if display_abstracts:
                                                         abstract = abstracts_list[i - 1]
