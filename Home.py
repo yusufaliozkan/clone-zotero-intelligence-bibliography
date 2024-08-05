401cd35e3aff28a840660780db18257f5cf62790
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-05 12:06:27 +0100

    ds

--- Home.py
+++ Home.py
@@ -4,6 +4,7 @@ import pandas as pd
 # home_page = st.Page('Home.py', title='Affiliation finder')
 
 home = st.Page('Home_page.py', title='Home')
+
 intelligence_history = st.Page('pages/1_Intelligence history.py', title='Intelligence history')
 intelligence_studies = st.Page('pages/2_Intelligence studies.py', title='Intelligence studies')
 intelligence_analysis = st.Page('pages/3_Intelligence analysis.py', title='Intelligence analysis')
@@ -16,6 +17,11 @@ covert_action = st.Page('pages/9_Covert action.py')
 intelligence_cybersphere = st.Page('pages/10_Intelligence and cybersphere.py')
 global_intelligence = st.Page('pages/11_Global intelligence.py')
 special_collections = st.Page('pages/13_Special collections.py')
+
+events = st.Page('pages/14_Events.py')
+digest = st.Page('pages/15_Digest.py')
+institutions = st.Page('16_Institutions.py')
+item_monitoring = st.Page('17_Item monitoring.py')
 
 pg = st.navigation(
     {
@@ -33,7 +39,13 @@ pg = st.navigation(
             intelligence_cybersphere,
             global_intelligence,
             special_collections
-            ]
+            ],
+        'Other resources':[
+            events,
+            digest,
+            institutions,
+            item_monitoring
+        ]
     }
 )
     
