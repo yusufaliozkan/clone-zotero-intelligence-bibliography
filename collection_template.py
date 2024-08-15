56018011070a7a82324abcd15632ca9d67875bb4
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-15 18:40:08 +0100

    sd

--- collection_template.py
+++ collection_template.py
@@ -30,7 +30,8 @@ import requests
 from st_keyup import st_keyup
 
 
-def collection_template(df_collections):
+def collection_template():
+        global df_collections
         def remove_numbers(name):
             return re.sub(r'^\d+(\.\d+)*\s*', '', name)
 
