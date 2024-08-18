870b013382228f9c512987c03e9f8c6845264c34
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-18 15:52:56 +0100

    df

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -206,7 +206,7 @@ else:
             df_db['Date published'] = df_db['Date published'].fillna('')
             df_db = df_db.sort_values(by=['Date published'], ascending=False)
             df_db = df_db.drop(columns=['Unnamed: 0'], inplace=True)
-            df_db = df_db.reset_index(drop=True)
+            # df_db = df_db.reset_index(drop=True)
             df_db['Include?'] = False
             last_column = df_db.columns[-1]
             df_db = df_db[[last_column] + list(df_db.columns[:-1])]
