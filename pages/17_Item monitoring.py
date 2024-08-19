5a4fad9c47fa4058a8749d15431d00282a91655c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-19 19:47:19 +0100

    ds

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -459,7 +459,7 @@ else:
 
             df_con = df_con[df_con['Include?']==True]
             df_con = df_con.reset_index(drop=True)
-            df_con = df_con[['event_name', 'organiser', 'link', 'venue', 'date_new']]
+            df_con = df_con[['conference_name', 'organiser', 'link', 'venue', 'date_new']]
             df_con
 
             post_events_bluesky = st.button('Post events on Bluesky')
