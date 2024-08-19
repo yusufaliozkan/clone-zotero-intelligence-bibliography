993e09d5727e7c991dbf37477033818b5cb91a08
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-19 19:45:16 +0100

    sd

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -450,12 +450,9 @@ else:
             df_con['date'] = pd.to_datetime(df_con['date'])
             df_con['date_new'] = df_con['date'].dt.strftime('%Y-%m-%d')
             df_con['date_new'] = pd.to_datetime(df_con['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
+            df_con = df_con[df_con['date_new'] >= pd.to_datetime('today').strftime('%Y-%m-%d')]
             df_con
-            df_con['date_new_end'] = pd.to_datetime(df_con['date_end'], dayfirst = True).dt.strftime('%d/%m/%Y')
-            df_con.sort_values(by='date', ascending = True, inplace=True)
-            df_con['details'] = df_con['details'].fillna('No details')
-            df_con['location'] = df_con['location'].fillna('No details')
-            df_con = df_con.fillna('')
+
             df_con['date_end'] = pd.to_datetime(df_con['date'], dayfirst=True) 
             filter = df_con['date_end']>=pd.to_datetime(today)
             df_con = df_con.loc[filter]
