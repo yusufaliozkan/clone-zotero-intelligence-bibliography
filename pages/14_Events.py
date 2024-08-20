b7d4e914826da7b5188e5e22124d15a1949412d2
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-20 11:01:59 +0100

    ds

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -71,7 +71,7 @@ with tab1:
     df_forms2 = df_forms.copy()
     df_forms2.sort_values(by='date_new', ascending = False, inplace=True)
     df_forms2['Timestamp'] = pd.to_datetime(df_forms2['Timestamp'], format='%m/%d/%Y %H:%M:%S')
-    df_forms2['Timestamp'] = df_forms2['Timestamp'].dt.strftime('%Y-%m-%d %H:%M')
+    df_forms2['Timestamp'] = df_forms2['Timestamp'].dt.strftime('%d-%m-%Y %H:%M')
     container.write('The events page last updated on ' + '**'+ df_forms2.iloc[0]['Timestamp']+'**')
     
     df_forms['date_new'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
