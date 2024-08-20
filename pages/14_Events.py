9f356190df3cbdb6075d7d25bde80ebb270ebff7
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-20 10:54:02 +0100

    ds

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -69,6 +69,8 @@ with tab1:
     df_forms.sort_values(by='date', ascending=True, inplace=True)
     df_forms = df_forms.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
     df_forms2 = df_forms.copy()
+    df_forms2.sort_values(by='date_new', ascending = True, inplace=True)
+    container.write('The events page last updated on ' + '**'+ df_forms2.loc[0]['date_new']+'**')
     
     df_forms['date_new'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
     df_forms['month'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%m')
@@ -76,8 +78,7 @@ with tab1:
     df_forms['month_year'] = pd.to_datetime(df_forms['date'], dayfirst = True).dt.strftime('%Y-%m')
     df_forms.sort_values(by='date', ascending = True, inplace=True)
     df_forms = df_forms.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
-    df_forms
-    container.write('The events page last updated on ' + '**'+ df_forms.loc[0]['date_new']+'**')
+ 
     
     df_forms['details'] = df_forms['details'].fillna('No details')
     df_forms = df_forms.fillna('')
