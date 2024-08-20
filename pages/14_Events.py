27f2e975dc52327603fee07cc026b8d20336c081
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-20 12:38:38 +0100

    ds

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -87,6 +87,7 @@ with tab1:
     df_forms = df_forms.fillna('')
     df_forms = df_forms.sort_index(ascending=True)
     df_gs = pd.concat([df_gs, df_forms], axis=0)
+    df_gs
     df_gs = df_gs.reset_index(drop=True)
     df_gs = df_gs.drop_duplicates(subset=['event_name', 'link', 'date'], keep='first')
     df_gs_plot = df_gs.copy()
