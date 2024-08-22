b62fe5c681244a2392c1f4aa6f53402b5dbb4bef
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-23 00:12:13 +0100

    sd

--- bluesky_bot.py
+++ bluesky_bot.py
@@ -1,9 +1,8 @@
+import os
 from atproto import Client
 import pandas as pd
 from pyzotero import zotero
 import numpy as np
-import os
-import re
 import requests
 from typing import List, Dict
 from bs4 import BeautifulSoup
@@ -12,7 +11,7 @@ from datetime import datetime, timedelta
 import pytz
 
 client = Client(base_url='https://bsky.social')
-bluesky_password = st.secrets["bluesky_password"]
+bluesky_password = os.getenv("BLUESKY_PASSWORD")
 client.login('intelbase.bsky.social', bluesky_password)
 
 post_content = 'Check out my profile: https://bsky.app/profile/intelbase.bsky.social'
@@ -20,25 +19,25 @@ post = client.send_post(post_content)
 
 def mark_urls(text):
     import re
-    
+
     # Define the regex pattern for URLs
     regex = r'(https?://[^\s]+)'
     matches = re.finditer(regex, text)
-    
+
     url_data = []
-    
+
     # Find all matches and store their positions and URLs
     for match in matches:
         url = match.group(0)
         start = match.start()
         end = match.end()
-        
+
         url_data.append({
             'start': start,
             'end': end,
             'url': url,
         })
-    
+
     return url_data
 
 
