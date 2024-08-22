9c1e71a79707ee2256b1fc2605c004524bd94ad5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-23 00:47:17 +0100

    sd

--- bluesky_bot.py
+++ bluesky_bot.py
@@ -199,3 +199,153 @@ def truncate_text(text: str, max_length: int) -> str:
         return text
     else:
         return text[:max_length-3] + "..."  # Reserve space for the ellipsis
+
+library_id = '2514686'
+library_type = 'group'
+api_key = '' # api_key is only needed for private groups and libraries
+
+zot = zotero.Zotero(library_id, library_type)
+def zotero_data(library_id, library_type):
+    items = zot.top(limit=50)
+    items = sorted(items, key=lambda x: x['data']['dateAdded'], reverse=True)
+    data=[]
+    columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Col key', 'Authors', 'Pub_venue', 'Book_title', 'Thesis_type', 'University']
+
+    for item in items:
+        creators = item['data']['creators']
+        creators_str = ", ".join([
+            creator.get('firstName', '') + ' ' + creator.get('lastName', '')
+            if 'firstName' in creator and 'lastName' in creator
+            else creator.get('name', '') 
+            for creator in creators
+        ])
+        data.append((item['data']['title'], 
+        item['data']['itemType'], 
+        item['data']['url'], 
+        item['data']['abstractNote'], 
+        item['links']['alternate']['href'],
+        item['data']['dateAdded'],
+        item['data'].get('date'), 
+        item['data']['dateModified'],
+        item['data']['collections'],
+        creators_str,
+        item['data'].get('publicationTitle'),
+        item['data'].get('bookTitle'),
+        item['data'].get('thesisType', ''),
+        item['data'].get('university', '')
+        ))
+    df = pd.DataFrame(data, columns=columns)
+    return df
+df = zotero_data(library_id, library_type)
+df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
+df['Abstract'] = df['Abstract'].fillna('No abstract')
+
+split_df= pd.DataFrame(df['Col key'].tolist())
+df = pd.concat([df, split_df], axis=1)
+df['Authors'] = df['Authors'].fillna('null')  
+
+# Change type name
+type_map = {
+    'thesis': 'Thesis',
+    'journalArticle': 'Journal article',
+    'book': 'Book',
+    'bookSection': 'Book chapter',
+    'blogPost': 'Blog post',
+    'videoRecording': 'Video',
+    'podcast': 'Podcast',
+    'magazineArticle': 'Magazine article',
+    'webpage': 'Webpage',
+    'newspaperArticle': 'Newspaper article',
+    'report': 'Report',
+    'forumPost': 'Forum post',
+    'conferencePaper' : 'Conference paper',
+    'audioRecording' : 'Podcast',
+    'preprint':'Preprint',
+    'document':'Document',
+    'computerProgram':'Computer program',
+    'dataset':'Dataset'
+}
+
+mapping_thesis_type ={
+    "MA Thesis": "Master's Thesis",
+    "PhD Thesis": "PhD Thesis",
+    "Master Thesis": "Master's Thesis",
+    "Thesis": "Master's Thesis",  # Assuming 'Thesis' refers to Master's Thesis here, adjust if necessary
+    "Ph.D.": "PhD Thesis",
+    "Master's Dissertation": "Master's Thesis",
+    "Undergraduate Theses": "Undergraduate Thesis",
+    "MPhil": "MPhil Thesis",
+    "A.L.M.": "Master's Thesis",  # Assuming A.L.M. (Master of Liberal Arts) maps to Master's Thesis
+    "doctoralThesis": "PhD Thesis",
+    "PhD": "PhD Thesis",
+    "Masters": "Master's Thesis",
+    "PhD thesis": "PhD Thesis",
+    "phd": "PhD Thesis",
+    "doctoral": "PhD Thesis",
+    "Doctoral": "PhD Thesis",
+    "Master of Arts Dissertation": "Master's Thesis",
+    "":'Unclassified'
+}
+df['Thesis_type'] = df['Thesis_type'].replace(mapping_thesis_type)
+df['Publication type'] = df['Publication type'].replace(type_map)
+df['Date published'] = (
+    df['Date published']
+    .str.strip()
+    .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
+)
+df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
+df['Date published'] = df['Date published'].fillna('No date')
+# df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
+
+# df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce')
+# df['Date added'] = df['Date added'].dt.strftime('%d-%m-%Y')
+df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce', utc=True)
+
+# df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
+# df['Date modified'] = df['Date modified'].dt.strftime('%d/%m/%Y, %H:%M')
+
+# today = datetime.now(pytz.UTC).date()
+# days_ago = today - timedelta(days=3)
+
+# df = df[df['Date added'].dt.date >= days_ago]
+
+now = datetime.now(pytz.UTC)
+last_hours = now - timedelta(hours=1)
+df = df[df['Date added'] >= last_hours]
+df = df[['Title', 'Publication type', 'Link to publication', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Authors']]
+
+for index, row in df.iterrows():
+    publication_type = row['Publication type']
+    title = row['Title']
+    publication_date = row['Date published']
+    link = row['Link to publication']
+    author_name = row['Authors']  # Extract the author name
+
+    post_text = f"{header}{publication_type}: {title} by {author_name} (published {publication_date})\n\n{link}"
+
+    if len(post_text) > 300:
+        max_title_length = 300 - len(f"{publication_type}: \n{link}") - len(f" (published {publication_date})")
+        truncated_title = truncate_text(title, max_title_length)
+        post_text = f"{header}{publication_type}: {truncated_title} (published {publication_date})\n{link}"
+
+    # Make sure the entire post_text fits within 300 graphemes
+    post_text = truncate_text(post_text, 300)
+
+    parsed = parse_facets_and_embed(post_text, client)
+    
+    post_payload = {
+        "$type": "app.bsky.feed.post",
+        "text": post_text,
+        "facets": parsed['facets'],
+        "embed": parsed['embed'],  # Include the embed if present
+        "createdAt": pd.Timestamp.utcnow().isoformat() + "Z"
+    }
+
+    try:
+        post = client.send_post(
+            text=post_payload["text"],  
+            facets=post_payload["facets"],  
+            embed=post_payload.get("embed"),  # Pass the embed if it exists
+        )
+    except Exception as e:
+        print(f"Failed to post: {e}")
