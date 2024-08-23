7bc2e21da71e2d4b1d785c9ed45ac0250133dff5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-23 11:02:43 +0100

    ds

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -286,6 +286,7 @@ with tab2:
     df_con_v2 = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=312814443')
     df_con_v2['date'] = pd.to_datetime(df_con_v2['date'])
     df_con_v2['date'] = df_con_v2['date'].dt.strftime('%Y-%m-%d')
+    df_con_v2['date'] = pd.to_datetime(df_con_v2['date'], dayfirst = True).dt.strftime('%d/%m/%Y')
     df_con_v2['date_end'] = pd.to_datetime(df_con_v2['date_end'])
     df_con_v2['date_end'] = df_con_v2['date_end'].dt.strftime('%Y-%m-%d')
     df_con_v2['date_end'] = pd.to_datetime(df_con_v2['date_end'], dayfirst = True).dt.strftime('%d/%m/%Y')
