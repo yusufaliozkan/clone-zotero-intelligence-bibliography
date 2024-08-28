c90e324cd7efce62a2cd093a7acc19ec54d3bfe0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-28 10:25:08 +0100

    sd

--- bluesky_posts/bluesky_events_bot.py
+++ bluesky_posts/bluesky_events_bot.py
@@ -169,11 +169,10 @@ df_forms = df_forms.drop_duplicates(subset=['event_name', 'link', 'date'], keep=
 
 conf_sheet_url_1 = "https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/export?format=csv&gid=939232836"
 df_con = pd.read_csv(conf_sheet_url_1)
-df_con['date'] = pd.to_datetime(df_con['date'], format='%d %b %Y', errors='coerce')
+df_con['date'] = pd.to_datetime(df_con['date'], errors='coerce')
 df_con['date_new'] = df_con['date'].dt.strftime('%Y-%m-%d')
 df_con = df_con[df_con['date'] == end_date]
 df_con = df_con.rename(columns={'conference_name':'event_name'})
-
 
 # df_con['date_new'] = pd.to_datetime(df_con['date'], dayfirst = True).dt.strftime('%Y-%m-%d')
 # df_con = df_con[df_con['date_new'] >= pd.to_datetime('today').strftime('%Y-%m-%d')]
