d2228990e0a1b86670973360e2512ba97d26bd53
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-08-28 10:53:35 +0100

    sd

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -467,8 +467,13 @@ else:
 
                 start_date = pd.to_datetime('today').normalize()
                 end_date = start_date + pd.Timedelta(days=0)
-                end_date
-
+                conf_sheet_url_1 = "https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/export?format=csv&gid=312814443"
+                df_con = pd.read_csv(conf_sheet_url_1)
+                df_con['date'] = pd.to_datetime(df_con['date'], errors='coerce')
+                df_con['date_new'] = df_con['date'].dt.strftime('%Y-%m-%d')
+                df_con = df_con[df_con['date'] == end_date]
+                df_con = df_con.rename(columns={'conference_name':'event_name'})
+                df_con
 
                 df_con = conn.read(spreadsheet='https://docs.google.com/spreadsheets/d/10ezNUOUpzBayqIMJWuS_zsvwklxP49zlfBWsiJI6aqI/edit#gid=939232836')
                 df_con['date'] = pd.to_datetime(df_con['date'])
