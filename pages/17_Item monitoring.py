70354c844b00c3e521ed7de3aa62f9621ed766f3
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-09-09 07:48:00 +0100

    ds

--- pages/17_Item monitoring.py
+++ pages/17_Item monitoring.py
@@ -112,7 +112,7 @@ def parse_mentions(text: str) -> List[Dict]:
 
 def parse_urls(text: str) -> List[Dict]:
     spans = []
-    url_regex = rb"[$|\W](https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*[-a-zA-Z0-9@%_\+~#//=])?)"
+    url_regex = rb"(https?://[^\s]+)"
     text_bytes = text.encode("UTF-8")
     for m in re.finditer(url_regex, text_bytes):
         spans.append({
