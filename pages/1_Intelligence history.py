db819d6eb1fb4e65e16b1a801e484ae64bfc8ecd
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-09-27 10:49:51 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -358,6 +358,7 @@ with st.spinner('Retrieving data & updating dashboard...'):
                     df_zotero_id = pd.read_csv('zotero_citation_format.csv')
                     df_collections = pd.merge(df_collections, df_zotero_id, on='zotero_item_key', how='left')
                     df_zotero_id = df_collections[['zotero_item_key']]
+                    df_zotero_id
 
                     def display_bibliographies(df):
                         df['bibliography'] = df['bibliography'].fillna('').astype(str)
