d5dbb88002ca279d3b40f1564858ca0910a8e65c
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-14 14:05:37 +0100

    ds

--- authors_dict.py
+++ authors_dict.py
@@ -66,6 +66,7 @@ name_replacements = {
     'James J Wirtz':'James J. Wirtz',
     'Philip HJ Davies':'Philip H. J. Davies',
     'Jeff Rogg':'Jeffrey P. Rogg',
-    'Celia G. Parker':'Celia G. Parker-Vincent'
+    'Celia G. Parker':'Celia G. Parker-Vincent',
+    'Celia Parker-Vincent':'Celia G. Parker-Vincent'
 }
 df_authors['Author_name'] = df_authors['Author_name'].map(name_replacements).fillna(df_authors['Author_name']) 
