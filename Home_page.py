cea6c80350cfd5610c0639c45733747e5ed0f5d0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-15 15:24:45 +0100

    sd

--- Home_page.py
+++ Home_page.py
@@ -242,6 +242,9 @@ with st.spinner('Retrieving data...'):
     df_dedup = pd.read_csv('all_items.csv')
     df_duplicated = pd.read_csv('all_items_duplicated.csv')
 
+    ttt = df_dedup[df_dedup['Title']=="Why the State Department's intelligence agency may be the best in DC"]
+    ttt
+
     col1, col2, col3 = st.columns([3,5,8])
     with col3:
         with st.expander('Introduction'):
@@ -670,8 +673,6 @@ with st.spinner('Retrieving data...'):
                                 search_tokens = parse_search_terms(search_term)
                                 print(f"Search Tokens: {search_tokens}")  # Debugging: Print search tokens
                                 df_csv = df_duplicated.copy()
-                                ttt = df_csv[df_csv['Title']=="Why the State Department's intelligence agency may be the best in DC"]
-                                ttt
 
                                 filtered_df = apply_boolean_search(df_csv, search_tokens, st.session_state.search_in)
                                 print(f"Filtered DataFrame (before dropping duplicates):\n{filtered_df}")  # Debugging: Print DataFrame before dropping duplicates
