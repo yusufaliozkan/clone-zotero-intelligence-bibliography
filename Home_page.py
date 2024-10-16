e7bd6d9c1463313148d5974e1cca9ac7469dea32
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-16 14:04:42 +0100

    sd

--- Home_page.py
+++ Home_page.py
@@ -1579,6 +1579,7 @@ with st.spinner('Retrieving data...'):
                                     collection_author_df.reset_index(drop=True, inplace=True)
                                     # from authors_dict import name_replacements
                                     df_authors, name_replacements = process_authors(df_authors)
+                                    df_authors
                                     name_replacements
                                     collection_author_df['Author_name'] = collection_author_df['Author_name'].map(name_replacements).fillna(collection_author_df['Author_name'])
                                     collection_author_df = collection_author_df['Author_name'].value_counts().head(10)
