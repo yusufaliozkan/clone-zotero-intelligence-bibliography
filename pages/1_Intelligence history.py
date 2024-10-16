c6adb3d106ecf4c0f6a3b30d51d426b9bbd5830f
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-16 14:56:26 +0100

    sd

--- pages/1_Intelligence history.py
+++ pages/1_Intelligence history.py
@@ -1,34 +1,34 @@
-from pyzotero import zotero
-import pandas as pd
-import streamlit as st
-from IPython.display import HTML
-import streamlit.components.v1 as components
-import numpy as np
-import altair as alt
-# from pandas.io.json import json_normalize
-import datetime
-import plotly.express as px
-import numpy as np
-import re
-import matplotlib.pyplot as plt
-import nltk
-nltk.download('all')
-from nltk.corpus import stopwords
-nltk.download('stopwords')
-from wordcloud import WordCloud
-# from gsheetsdb import connect
-from streamlit_gsheets import GSheetsConnection
-import datetime as dt     
-import random
-from authors_dict import process_authors
-from sidebar_content import sidebar_content, set_page_config
-from format_entry import format_entry
-from copyright import display_custom_license
-from events import evens_conferences
-from streamlit_dynamic_filters import DynamicFilters
-import requests
-from st_keyup import st_keyup
-from collection_template import collection_template 
+# from pyzotero import zotero
+# import pandas as pd
+# import streamlit as st
+# from IPython.display import HTML
+# import streamlit.components.v1 as components
+# import numpy as np
+# import altair as alt
+# # from pandas.io.json import json_normalize
+# import datetime
+# import plotly.express as px
+# import numpy as np
+# import re
+# import matplotlib.pyplot as plt
+# import nltk
+# nltk.download('all')
+# from nltk.corpus import stopwords
+# nltk.download('stopwords')
+# from wordcloud import WordCloud
+# # from gsheetsdb import connect
+# from streamlit_gsheets import GSheetsConnection
+# import datetime as dt     
+# import random
+# from authors_dict import process_authors
+# from sidebar_content import sidebar_content, set_page_config
+# from format_entry import format_entry
+# from copyright import display_custom_license
+# from events import evens_conferences
+# from streamlit_dynamic_filters import DynamicFilters
+# import requests
+# from st_keyup import st_keyup
+# from collection_template import collection_template 
 
 
 set_page_config()
