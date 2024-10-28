d27035b831d23a9ccc246c9c43587bb7abcccaec
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-28 13:32:45 +0000

    sd

--- pages/11_Global intelligence.py
+++ pages/11_Global intelligence.py
@@ -786,6 +786,9 @@ with st.spinner('Retrieving data & updating dashboard...'):
             country_pub_counts = df_countries_chart['Country'].value_counts().sort_values(ascending=False)
             all_countries_df = pd.DataFrame({'Country': country_pub_counts.index, 'Publications': country_pub_counts.values})
 
+            df_continent = df_continent.copy()
+            df_continent_chart = df_continent.copy() 
+
             # Function to get coordinates
             def get_coordinates(country_name):
                 try:
