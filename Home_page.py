a4e0a40f7107bf1564506626e3e93270018476b5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-28 07:45:14 +0000

    sd

--- Home_page.py
+++ Home_page.py
@@ -4481,6 +4481,7 @@ with st.spinner('Retrieving data...'):
                     st.pydeck_chart(chart, use_container_width=True)
                 with col2:
                     fig = px.bar(df_countries.head(15).iloc[::-1], x='# Mentions', y='Country', orientation='h', height=600)
+                    df_countries = df_countries.reset_index()
                     st.dataframe(df_countries, height=500)
                     # col2.plotly_chart(fig, use_container_width=True)
                 
