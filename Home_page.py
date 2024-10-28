ff394eadb04ea648e8ae87dec8b9702758beee08
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-28 07:16:51 +0000

    ds

--- Home_page.py
+++ Home_page.py
@@ -4413,6 +4413,7 @@ with st.spinner('Retrieving data...'):
                 # Load your country data with counts
                 df_countries = pd.read_csv('countries.csv')
                 df_countries['Country'] = df_countries['Country'].replace("UK", "United Kingdom")
+                df_countries
 
                 # Function to get coordinates
                 def get_coordinates(country_name):
