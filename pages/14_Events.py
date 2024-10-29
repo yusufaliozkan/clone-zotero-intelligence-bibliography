fb71ec8963a0d4db7724bdc9257d32fa05db0876
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-10-29 07:09:27 +0000

    sd

--- pages/14_Events.py
+++ pages/14_Events.py
@@ -212,6 +212,7 @@ with tab1:
     selector = st.checkbox('Select a year')
     year = st.checkbox('Show years only')
     if selector:
+        df_gs
         max_year = df_gs['date'].dt.year.max()
         min_year = df_gs['date'].dt.year.min()
         current_year = pd.Timestamp.now().year
