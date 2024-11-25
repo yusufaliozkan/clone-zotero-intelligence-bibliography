182e8796b8df412cfa82e22fe98b084866eb2d76
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2024-11-25 17:24:24 +0000

    sd

--- Home_page.py
+++ Home_page.py
@@ -565,14 +565,14 @@ with st.spinner('Retrieving data...'):
                     5: "Publication year",
                     6: "Cited papers"
                 }
-                selection = st.pills(
+                search_option = st.pills(
                     "Select search option",
                     options=option_map.keys(),
                     format_func=lambda option: option_map[option],
                     selection_mode="single",
                 )
 
-                search_option = st.radio("Select search option", ("Search keywords", "Search author", "Search collection", "Publication types", "Search journal", "Publication year", "Cited papers"), horizontal=True)
+                # search_option = st.radio("Select search option", ("Search keywords", "Search author", "Search collection", "Publication types", "Search journal", "Publication year", "Cited papers"), horizontal=True)
                 if search_option == "Search keywords":
                     st.subheader('Search keywords', anchor=False, divider='blue')
                     @st.experimental_fragment
