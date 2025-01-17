f45bd3457b858d97f701567e84433236547909d0
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-01-17 09:16:01 +0000

    sd

--- Home_page.py
+++ Home_page.py
@@ -207,7 +207,7 @@ df = df.fillna('')
 
 # Streamlit app
 st.image('https://github.com/yusufaliozkan/clone-zotero-intelligence-bibliography/blob/main/images/IntelArchive_Digital_Logo_Colour-Negative.png')
-st.title("IntelArchive", anchor=False)
+st.title("IntelArchives", anchor=False)
 st.subheader('Intelligence Studies Database', anchor=False)
 # st.header("[Zotero group library](https://www.zotero.org/groups/2514686/intelligence_bibliography/library)")
 
