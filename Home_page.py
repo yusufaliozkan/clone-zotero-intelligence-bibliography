d9132db97ee0941e200b93afe7cc619a638f02fe
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-15 19:14:07 +0000

    rwe

--- Home_page.py
+++ Home_page.py
@@ -276,8 +276,7 @@ with st.spinner('Retrieving data...'):
         ]        # st.write(f'**{item_count}** items available in this library. **{len(items_added_this_month)}** items added in {current_date.strftime("%B %Y")}.')
         st.metric(label='Number of items in the library', value=item_count, delta=len(items_added_this_month),label_visibility='visible', help=f' **{len(items_added_this_month)}** items added in {current_date.strftime("%B %Y")}')
     df_intro
-    st.write('The library last updated on ' + '**' + df_intro.loc[0]['Date added'].strftime('%Y-%m-%d') + '**')
-
+    st.write('The library last updated on ' + '**' + df_intro.loc[0]['Date added'].strftime('%d/%m/%Y, %H:%M') + '**')
     st.write('The library last updated on ' + '**'+ df.loc[0]['Date modified']+'**')
     df_dedup_oa = df_dedup[df_dedup['OA status'] == True].reset_index(drop=True)
 
