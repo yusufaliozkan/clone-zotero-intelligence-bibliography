a193251eae90e47e9722d48b8e5cc538de3de239
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-03-15 21:50:49 +0000

    sd

--- Home_page.py
+++ Home_page.py
@@ -59,104 +59,104 @@ pd.set_option('display.max_colwidth', None)
 zot = zotero.Zotero(library_id, library_type)
 # aa = zot.top(limit=10)
 # aa
-@st.cache_data(ttl=600)
-def zotero_data(library_id, library_type):
-    items = zot.top(limit=10)
-    items = sorted(items, key=lambda x: x['data']['dateAdded'], reverse=True)
-    data=[]
-    columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Col key', 'Authors', 'Pub_venue', 'Book_title', 'Thesis_type', 'University']
+# @st.cache_data(ttl=600)
+# def zotero_data(library_id, library_type):
+#     items = zot.top(limit=10)
+#     items = sorted(items, key=lambda x: x['data']['dateAdded'], reverse=True)
+#     data=[]
+#     columns = ['Title','Publication type', 'Link to publication', 'Abstract', 'Zotero link', 'Date added', 'Date published', 'Date modified', 'Col key', 'Authors', 'Pub_venue', 'Book_title', 'Thesis_type', 'University']
 
-    for item in items:
-        creators = item['data']['creators']
-        creators_str = ", ".join([
-            creator.get('firstName', '') + ' ' + creator.get('lastName', '')
-            if 'firstName' in creator and 'lastName' in creator
-            else creator.get('name', '') 
-            for creator in creators
-        ])
-        data.append((item['data']['title'], 
-        item['data']['itemType'], 
-        item['data']['url'], 
-        item['data']['abstractNote'], 
-        item['links']['alternate']['href'],
-        item['data']['dateAdded'],
-        item['data'].get('date'), 
-        item['data']['dateModified'],
-        item['data']['collections'],
-        creators_str,
-        item['data'].get('publicationTitle'),
-        item['data'].get('bookTitle'),
-        item['data'].get('thesisType', ''),
-        item['data'].get('university', '')
-        ))
-    df = pd.DataFrame(data, columns=columns)
-    return df
-df = zotero_data(library_id, library_type)
+#     for item in items:
+#         creators = item['data']['creators']
+#         creators_str = ", ".join([
+#             creator.get('firstName', '') + ' ' + creator.get('lastName', '')
+#             if 'firstName' in creator and 'lastName' in creator
+#             else creator.get('name', '') 
+#             for creator in creators
+#         ])
+#         data.append((item['data']['title'], 
+#         item['data']['itemType'], 
+#         item['data']['url'], 
+#         item['data']['abstractNote'], 
+#         item['links']['alternate']['href'],
+#         item['data']['dateAdded'],
+#         item['data'].get('date'), 
+#         item['data']['dateModified'],
+#         item['data']['collections'],
+#         creators_str,
+#         item['data'].get('publicationTitle'),
+#         item['data'].get('bookTitle'),
+#         item['data'].get('thesisType', ''),
+#         item['data'].get('university', '')
+#         ))
+#     df = pd.DataFrame(data, columns=columns)
+#     return df
+# df = zotero_data(library_id, library_type)
 
-df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
-df['Abstract'] = df['Abstract'].fillna('No abstract')
+# df['Abstract'] = df['Abstract'].replace(r'^\s*$', np.nan, regex=True) # To replace '' with NaN. Otherwise the code below do not understand the value is nan.
+# df['Abstract'] = df['Abstract'].fillna('No abstract')
 
-split_df= pd.DataFrame(df['Col key'].tolist())
-df = pd.concat([df, split_df], axis=1)
-df['Authors'] = df['Authors'].fillna('null')  
+# split_df= pd.DataFrame(df['Col key'].tolist())
+# df = pd.concat([df, split_df], axis=1)
+# df['Authors'] = df['Authors'].fillna('null')  
 
-# Change type name
-type_map = {
-    'thesis': 'Thesis',
-    'journalArticle': 'Journal article',
-    'book': 'Book',
-    'bookSection': 'Book chapter',
-    'blogPost': 'Blog post',
-    'videoRecording': 'Video',
-    'podcast': 'Podcast',
-    'magazineArticle': 'Magazine article',
-    'webpage': 'Webpage',
-    'newspaperArticle': 'Newspaper article',
-    'report': 'Report',
-    'forumPost': 'Forum post',
-    'conferencePaper' : 'Conference paper',
-    'audioRecording' : 'Podcast',
-    'preprint':'Preprint',
-    'document':'Document',
-    'computerProgram':'Computer program',
-    'dataset':'Dataset'
-}
+# # Change type name
+# type_map = {
+#     'thesis': 'Thesis',
+#     'journalArticle': 'Journal article',
+#     'book': 'Book',
+#     'bookSection': 'Book chapter',
+#     'blogPost': 'Blog post',
+#     'videoRecording': 'Video',
+#     'podcast': 'Podcast',
+#     'magazineArticle': 'Magazine article',
+#     'webpage': 'Webpage',
+#     'newspaperArticle': 'Newspaper article',
+#     'report': 'Report',
+#     'forumPost': 'Forum post',
+#     'conferencePaper' : 'Conference paper',
+#     'audioRecording' : 'Podcast',
+#     'preprint':'Preprint',
+#     'document':'Document',
+#     'computerProgram':'Computer program',
+#     'dataset':'Dataset'
+# }
 
-mapping_thesis_type ={
-    "MA Thesis": "Master's Thesis",
-    "PhD Thesis": "PhD Thesis",
-    "Master Thesis": "Master's Thesis",
-    "Thesis": "Master's Thesis",  # Assuming 'Thesis' refers to Master's Thesis here, adjust if necessary
-    "Ph.D.": "PhD Thesis",
-    "Master's Dissertation": "Master's Thesis",
-    "Undergraduate Theses": "Undergraduate Thesis",
-    "MPhil": "MPhil Thesis",
-    "A.L.M.": "Master's Thesis",  # Assuming A.L.M. (Master of Liberal Arts) maps to Master's Thesis
-    "doctoralThesis": "PhD Thesis",
-    "PhD": "PhD Thesis",
-    "Masters": "Master's Thesis",
-    "PhD thesis": "PhD Thesis",
-    "phd": "PhD Thesis",
-    "doctoral": "PhD Thesis",
-    "Doctoral": "PhD Thesis",
-    "Master of Arts Dissertation": "Master's Thesis",
-    "":'Unclassified'
-}
-df['Thesis_type'] = df['Thesis_type'].replace(mapping_thesis_type)
-df['Publication type'] = df['Publication type'].replace(type_map)
-df['Date published'] = (
-    df['Date published']
-    .str.strip()
-    .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
-)
-df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
-df['Date published'] = df['Date published'].fillna('No date')
-# df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
+# mapping_thesis_type ={
+#     "MA Thesis": "Master's Thesis",
+#     "PhD Thesis": "PhD Thesis",
+#     "Master Thesis": "Master's Thesis",
+#     "Thesis": "Master's Thesis",  # Assuming 'Thesis' refers to Master's Thesis here, adjust if necessary
+#     "Ph.D.": "PhD Thesis",
+#     "Master's Dissertation": "Master's Thesis",
+#     "Undergraduate Theses": "Undergraduate Thesis",
+#     "MPhil": "MPhil Thesis",
+#     "A.L.M.": "Master's Thesis",  # Assuming A.L.M. (Master of Liberal Arts) maps to Master's Thesis
+#     "doctoralThesis": "PhD Thesis",
+#     "PhD": "PhD Thesis",
+#     "Masters": "Master's Thesis",
+#     "PhD thesis": "PhD Thesis",
+#     "phd": "PhD Thesis",
+#     "doctoral": "PhD Thesis",
+#     "Doctoral": "PhD Thesis",
+#     "Master of Arts Dissertation": "Master's Thesis",
+#     "":'Unclassified'
+# }
+# df['Thesis_type'] = df['Thesis_type'].replace(mapping_thesis_type)
+# df['Publication type'] = df['Publication type'].replace(type_map)
+# df['Date published'] = (
+#     df['Date published']
+#     .str.strip()
+#     .apply(lambda x: pd.to_datetime(x, utc=True, errors='coerce').tz_convert('Europe/London'))
+# )
+# df['Date published'] = df['Date published'].dt.strftime('%d-%m-%Y')
+# df['Date published'] = df['Date published'].fillna('No date')
+# # df['Date published'] = df['Date published'].map(lambda x: x.strftime('%d/%m/%Y') if x else 'No date')
 
-df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce')
-df['Date added'] = df['Date added'].dt.strftime('%d/%m/%Y')
-df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
-df['Date modified'] = df['Date modified'].dt.strftime('%d/%m/%Y, %H:%M')
+# df['Date added'] = pd.to_datetime(df['Date added'], errors='coerce')
+# df['Date added'] = df['Date added'].dt.strftime('%d/%m/%Y')
+# df['Date modified'] = pd.to_datetime(df['Date modified'], errors='coerce')
+# df['Date modified'] = df['Date modified'].dt.strftime('%d/%m/%Y, %H:%M')
 
 # Bringing collections
 
