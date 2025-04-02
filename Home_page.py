86ee870663f1ca7e0f79c1a7a20866f64cc73c7d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-02 23:29:54 +0100

    ds

--- Home_page.py
+++ Home_page.py
@@ -3150,7 +3150,7 @@ with st.spinner('Retrieving data...'):
                     display = st.checkbox('Display abstract')
 
                     def format_row(row):
-                        pub_link = f"[:orange-badge[Publication link]]({row['Link to publication']})"
+                        pub_link = f"[:green-badge[Publication link]]({row['Link to publication']})"
                         zotero_link = f"[:red-badge[Zotero link]]({row['Zotero link']})"
 
                         if row['Publication type'] == 'Book chapter' and row['Book_title']:
