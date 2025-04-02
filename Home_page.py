bb1e5259d03a0b46b88996234029de2a526c518d
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-02 23:38:12 +0100

    sd

--- Home_page.py
+++ Home_page.py
@@ -3197,7 +3197,7 @@ with st.spinner('Retrieving data...'):
                         st.markdown(f"{i+1}) {formatted}")
 
                         if display and row['Abstract']:
-                            st.write("Abstract: " + row['Abstract'])
+                            st.markdown(f"Abstract:  + {row['Abstract']}")
 
                 with tab12:
                     st.markdown('#### Recently published items')
