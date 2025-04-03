cfbd96da55bd61dc7c0d54adb5002093b33b0fb5
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-04-03 12:51:14 +0100

    sd

--- format_entry.py
+++ format_entry.py
@@ -42,7 +42,7 @@ def format_entry(row, include_citation=True):
 
     pub_link_badge = f"[:blue-badge[Publication link]]({link_to_publication})" if link_to_publication else ''
     zotero_link_badge = f"[:blue-badge[Zotero link]]({zotero_link})" if zotero_link else ''
-    oa_link_text = f"[:violet-badge[OA version]]({oa_url_fixed})" if oa_url_fixed else ''
+    oa_link_text = f"[:green-badge[OA version]]({oa_url_fixed})" if oa_url_fixed else ''
     citation_text = f"[:orange-badge[Cited by {citation}]]({citation_link})" if citation > 0 else ''
 
 
