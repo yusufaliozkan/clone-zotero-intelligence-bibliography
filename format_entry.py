33ef64d4bb0fa44540bf06b9d4ac891dfb175bc8
Yusuf Ozkan <70472797+yusufaliozkan@users.noreply.github.com>
2025-09-24 16:03:16 +0100

    sd

--- format_entry.py
+++ format_entry.py
@@ -25,11 +25,11 @@ def format_entry(row, include_citation=True, reviews_map=None, max_reviews_inlin
     pub_link_badge    = f"[:blue-badge[Publication link]]({link_to_publication})" if link_to_publication else ""
     zotero_link_badge = f"[:blue-badge[Zotero link]]({zotero_link})" if zotero_link else ""
     oa_link_text      = f"[:green-badge[OA version]]({oa_url_fixed})" if oa_url_fixed else ""
-    if citation:
+    if citation > 0:
         if citation_link:
-            citation_text = f"[:orange-badge[Cited by {int(citation)}]]({citation_link})"
+            citation_text = f"[:orange-badge[Cited by {citation}]]({citation_link})"
         else:
-            citation_text = f":orange-badge[Cited by {int(citation)}]"
+            citation_text = f":orange-badge[Cited by {citation}]"
     else:
         citation_text = ""
 
